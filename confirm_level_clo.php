cd514ce117da2ad95171a306bf87ab0f89c374c7
MussabTanveer <mussab.tanveer9@gmail.com>
2017-11-30 23:47:58 +0500

    map CLO to Taxonomy Level

---
+++ confirm_level_clo.php
@@ -0,0 +1,70 @@
+<?php
+	require_once('../config.php');
+    $context = context_system::instance();
+	$PAGE->set_context($context);
+    $PAGE->set_pagelayout('admin');
+    $PAGE->set_title("Add Level");
+    $PAGE->set_heading("Add CLO Level");
+    $PAGE->set_url($CFG->wwwroot.'/custom/confirm_level_clo.php');
+    
+	echo $OUTPUT->header();
+	require_login();
+    is_siteadmin() || die('<h2>This page is for site admins only!</h2>'.$OUTPUT->footer());
+ 
+	if(isset($_POST['submit']))
+	{
+		$clocount = $_POST['clocount'];
+		$fw_id = $_POST['fwid'];
+
+		$levelarray = array();
+		foreach ($_POST['level'] as $level)
+		{
+			array_push($levelarray,$level);	
+		}
+	}
+	$cloidsarray=$SESSION->cloids;
+	/*
+	echo $cloidsarray[0];
+	echo "<br>";
+	echo $cloidsarray[1];
+	echo "<br>";
+	echo $cloidsarray[2];
+	echo "<br>";
+	echo $cloidsarray[3];
+	echo "<br>";
+	echo $cloidsarray[4];
+	echo "<br>";
+	echo $cloidsarray[5];
+	echo "<br>";echo "<br>";
+	
+	echo $levelarray[0];
+	echo "<br>";
+	echo $levelarray[1];
+	echo "<br>";
+	echo $levelarray[2];
+	echo "<br>";
+	echo $levelarray[3];
+	echo "<br>";
+	echo $levelarray[4];
+	echo "<br>";
+	echo $levelarray[5];
+	echo "<br>";echo "<br>";
+	*/
+	$i=0;
+	foreach ($cloidsarray as $cid)
+	{
+		if($levelarray[$i] != ''){
+			$sql="INSERT INTO mdl_taxonomy_clo_level (frameworkid, cloid, levelid) VALUES($fw_id, $cid, $levelarray[$i])";
+			$DB->execute($sql);
+		}
+		$i++;
+	}
+	
+	echo "<font color='green'>Levels successfully mapped with the CLOs!</font>";
+	echo "<br>";
+?>
+<a href="./display_outcome_framework-3.php">Back</a>
+
+<?php 	
+	echo $OUTPUT->footer();
+?>
---
+++ map_clo_level.php
@@ -0,0 +1,138 @@
+<?php 
+    require_once('../config.php');
+    $context = context_system::instance();
+    $PAGE->set_context($context);
+    $PAGE->set_pagelayout('admin');
+    $PAGE->set_title("CLOs and Levels");
+    $PAGE->set_heading("Map CLOs to Taxonomy Levels");
+    $PAGE->set_url($CFG->wwwroot.'/custom/map_clo_level.php');
+    
+    echo $OUTPUT->header();
+    require_login();
+    is_siteadmin() || die('<h2>This page is for site admins only!</h2>'.$OUTPUT->footer());
+
+    if(isset($_POST['submit']) && isset( $_POST['fwid']))
+    {
+        $fw_id=$_POST['fwid'];
+        //echo "FW ID : $fw_id";
+    ?>
+
+        <?php
+        // Get CLOs
+        $rec=$DB->get_records_sql("SELECT * FROM mdl_competency WHERE competencyframeworkid = ? AND idnumber NOT LIKE 'PLO%' AND parentid !=0 ORDER BY id", array($fw_id));
+
+        //Get level with its name and domain name
+        $recLevels=$DB->get_records_sql("SELECT txl.id, txl.name AS level_name, txl.level, txd.name AS domain_name FROM mdl_taxonomy_levels txl, mdl_taxonomy_domain txd WHERE txl.domainid=txd.id");
+        $levelid = array(); $lname = array(); $dname = array();
+        foreach ($recLevels as $recL) {
+            $lid = $recL->id;
+            $lvl = $recL->level;
+            $ln = $recL->level_name;
+            $dn = $recL->domain_name;
+            array_push($levelid, $lid); // array of level ids
+            array_push($lname, $ln); // array of level names
+            array_push($dname, $dn); // array of domain names
+        }
+
+        if($rec){
+            $i = 0;
+            $cloids = array();
+            ?>
+            <form action="confirm_level_clo.php" method="post">
+                <table class="generaltable">
+                    <tr class="table-head">
+                        <th> CLOs </th>
+                        <th> Taxonomy Level </th>
+                        <th> Level Name </th>
+                        <th> Domain Name </th>
+                    </tr>
+                    <?php
+                    $i = 0;
+                    foreach($rec as $records)
+                    {
+                        $cid = $records->id;
+                        $sname = $records->shortname;
+                        array_push($cloids,$cid);
+                    ?>
+                                
+                    <tr>
+                        <td><?php echo $sname;?> </td>
+                        <td>
+                            <select required onChange="dropdownTip(this.value, <?php echo $i ?>)" name="level[]" class="select custom-select">
+                                <option value=''>Choose..</option>
+                                <?php
+                                foreach ($recLevels as $recL) {
+                                    $lid = $recL->id;
+                                    $lvl = $recL->level;
+                                    ?>
+                                    <option value='<?php echo $lid; ?>'><?php echo $lvl; ?></option>
+                                <?php
+                                }
+                                ?>
+                            </select>
+                        </td>
+                        <td id="lname<?php echo $i ?>"></td>
+                        <td id="dname<?php echo $i ?>"></td>
+                    </tr>
+                    <?php
+                        $i++;
+                        }
+                        global $SESSION;
+                        $SESSION->cloids = $cloids;
+                        
+                        ?>
+                </table>
+			
+                <input type="hidden" value='<?php echo $i; ?>' name="clocount">
+                <input type="hidden" value='<?php echo $fw_id; ?>' name="fwid">
+                <input type="submit" value="NEXT" name="submit" class="btn btn-primary">
+	    	</form>
+            
+            <a href="./display_outcome_framework-3.php">Back</a>
+
+            <?php
+            echo $OUTPUT->footer();
+            ?>
+            <script>
+                var levelid = <?php echo json_encode($levelid); ?>;
+                var lnames = <?php echo json_encode($lname); ?>;
+                var dnames = <?php echo json_encode($dname); ?>;
+                /*alert(closid);
+                alert(plos);
+                alert(peos);*/
+                function dropdownTip(value,id){
+                    var lname = "lname" + id;
+                    var dname = "dname" + id;
+                    //console.log(value);
+                    //console.log(id);
+                    if(value == ''){
+                        document.getElementById(lname).innerHTML = "";
+                        document.getElementById(dname).innerHTML = "";
+                    }
+                    else{
+                        for(var i=0; i<levelid.length ; i++){
+                            if(levelid[i] == value){
+                                document.getElementById(lname).innerHTML = lnames[i];
+                                document.getElementById(dname).innerHTML = dnames[i];
+                                break;
+                            }
+                        }
+                    }
+                }
+            </script>
+            <?php
+        }
+        else{
+            echo "<h3>No CLOs found!</h3>";
+            echo '<a href="./display_outcome_framework-3.php">Back</a>';
+            echo $OUTPUT->footer();
+        }
+
+    }
+    else
+    {?>
+        <h2 style="color:red;"> Invalid Selection </h2>
+        <a href="./display_outcome_framework-3.php">Back</a>
+    <?php
+        echo $OUTPUT->footer();
+    }?>
