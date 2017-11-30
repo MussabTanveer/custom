b559a6edfaa4507522bf0ad055c361e357052c51
MussabTanveer <mussab.tanveer9@gmail.com>
2017-11-30 23:54:38 +0500

    add/edit CLO to framework forms

---
+++ view_clos.php
@@ -0,0 +1,38 @@
+<?php
+    require_once('../config.php');
+    $context = context_system::instance();
+    $PAGE->set_context($context);
+    $PAGE->set_pagelayout('admin');
+    $PAGE->set_title("View OBE PLOs");
+    $PAGE->set_heading("View CLOs");
+    $PAGE->set_url($CFG->wwwroot.'/custom/view_clos.php');
+    
+    echo $OUTPUT->header();
+	require_login();
+    is_siteadmin() || die('<h2>This page is for site admins only!</h2>'.$OUTPUT->footer());
+	
+	if(isset($_GET['fwid']))
+	{
+        $fw_id=$_GET['fwid'];
+
+        $clos=$DB->get_records_sql('SELECT * FROM `mdl_competency` WHERE competencyframeworkid = ? AND idnumber LIKE "%%-%%%-clo%"', array($fw_id));
+        
+        if($clos){
+            $i = 1;
+            echo "<h3>Already Present CLOs In Framework</h3>";
+            foreach ($clos as $records){
+                $shortname = $records->shortname;
+                $id=$records->id;
+                echo "<div class='row'><div class='col-md-2 col-sm-4 col-xs-8'>$i. $shortname</div> <div class='col-md-10 col-sm-8 col-xs-4'><a href='edit_clo.php?edit=$id&fwid=$fw_id' title='Edit'><img src='./img/icons/edit.png' /></a> <a href='delete_clo.php?delete=$id&fwid=$fw_id' title='Delete'><img src='./img/icons/delete.png' /></a></div></div>";//link to edit_plo.php 
+                $i++;
+            }
+        }
+        echo $OUTPUT->footer();
+	}
+	else
+	{?>
+    	<h3 style="color:red;"> Invalid Selection </h3>
+    	<a href="./select_frameworktoCLO.php">Back</a>
+    	<?php
+        echo $OUTPUT->footer();
+    }?>
