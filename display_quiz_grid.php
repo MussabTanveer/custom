c170c74c0789afdb1411b9fb283ebb7602a54e33
MussabTanveer <mussab.tanveer9@gmail.com>
2017-11-30 23:38:51 +0500

    quiz grid report of all students

---
+++ display_quiz_grid.php
@@ -0,0 +1,162 @@
+<?php 
+    require_once('../config.php');
+    $context = context_system::instance();
+    $PAGE->set_context($context);
+    $PAGE->set_pagelayout('admin');
+    $PAGE->set_title("Quiz Results");
+    $PAGE->set_heading("Results");
+    $PAGE->set_url($CFG->wwwroot.'/custom/display_quiz_grid.php');
+    
+    echo $OUTPUT->header();
+    require_login();
+
+    if(isset($_POST['submit']) && isset( $_POST['quizid']))
+    {
+        $quiz_id=$_POST['quizid'];
+        //echo "Quiz ID : $quiz_id";
+        ?>
+
+        <?php
+        // Display Quiz Info
+        echo "<h3>Question category wise grid</h3>";
+        $rec=$DB->get_recordset_sql(
+        'SELECT
+            qa.userid,
+            qa.attempt,
+            qu.name,
+			c.shortname,
+            qu.questiontext,
+            qua.rightanswer,
+            qua.responsesummary,
+            qua.maxmark,
+            qua.maxmark*qas.fraction AS marksobtained,
+            qc.name AS category
+
+        FROM
+            mdl_quiz q,
+            mdl_quiz_slots qs,
+            mdl_question qu,
+            mdl_question_categories qc,
+            mdl_quiz_attempts qa,
+            mdl_question_attempts qua,
+			mdl_competency c,
+            mdl_question_attempt_steps qas
+
+        WHERE
+            q.id=? AND q.id=qs.quizid AND qu.id=qs.questionid AND qu.category=qc.id AND q.id=qa.quiz AND c.id=qu.competencyid
+            AND qa.uniqueid=qua.questionusageid AND qu.id=qua.questionid AND qua.id=qas.questionattemptid AND qas.fraction IS NOT NULL
+
+        ORDER BY qa.attempt, qa.userid',
+        
+        array($quiz_id));
+
+        if($rec){
+            $table = new html_table();
+            $table->head = array('Student ID', 'No. of Attempts', 'Question Name','Competency', 'Question', 'Correct Ans', 'Student Ans', 'Max Marks', 'Marks Obtained', 'Question Category');
+            foreach ($rec as $records) {
+                $uid = $records->userid;
+                $attempt = $records->attempt;
+                $qname = $records->name;
+				$competency=$records->shortname;
+                $qtext = $records->questiontext;
+                $qrightans = $records->rightanswer;
+                $qresponse = $records->responsesummary;
+                $qmax = $records->maxmark; $qmax = number_format($qmax, 2); // 2 dp
+                $mobtained = $records->marksobtained; $mobtained = number_format($mobtained, 2);
+                $cname = $records->category;
+                $table->data[] = array($uid, $attempt, $qname,$competency ,$qtext, $qrightans, $qresponse, $qmax, $mobtained, $cname);
+            }
+            $rec->close(); // Don't forget to close the recordset!
+            echo html_writer::table($table);
+        }
+        else{
+            echo "<h3>No students have attempted the quiz!</h3>";
+        }
+
+        /*
+        SELECT 
+            mdl_quiz.id,
+            mdl_question.id,
+            mdl_question.questiontext,
+            mdl_question_categories.name
+        FROM
+            mdl_quiz
+            INNER JOIN mdl_quiz_slots ON mdl_quiz.id = mdl_quiz_slots.quizid
+            INNER JOIN mdl_question ON mdl_quiz_slots.questionid = mdl_question.id
+            INNER JOIN mdl_question_categories ON mdl_question.category = mdl_question_categories.id
+        WHERE 
+            mdl_quiz.id = 1
+                
+            
+        SELECT 
+            mdl_quiz.id,
+            mdl_quiz_attempts.userid
+        FROM
+            mdl_quiz
+            INNER JOIN mdl_quiz_attempts ON mdl_quiz.id = mdl_quiz_attempts.quiz
+        WHERE 
+            mdl_quiz.id = 1
+
+
+        SELECT
+            quiza.userid,
+            qa.questionid,
+            ques.name,
+            qa.maxmark,
+            qa.questionsummary,
+            qa.rightanswer,
+            qa.responsesummary,
+            qa.maxmark*qas.fraction AS marksobtained,
+            qc.name AS category
+
+        FROM mdl_quiz_attempts quiza
+        JOIN mdl_question_usages qu ON qu.id = quiza.uniqueid
+        JOIN mdl_question_attempts qa ON qa.questionusageid = qu.id
+        JOIN mdl_question_attempt_steps qas ON qas.questionattemptid = qa.id
+        LEFT JOIN mdl_question_attempt_step_data qasd ON qasd.attemptstepid = qas.id,
+        mdl_quiz q
+        JOIN mdl_quiz_slots qs ON q.id = qs.quizid
+        JOIN mdl_question ques ON qs.questionid = ques.id
+        JOIN mdl_question_categories qc ON ques.category = qc.id
+        
+        WHERE quiza.quiz = 1 AND qas.fraction IS NOT NULL AND q.id = 1
+        GROUP BY quiza.userid, qa.questionid
+        ORDER BY quiza.userid, qa.questionid
+
+        SELECT
+            qa.userid,
+            qa.attempt,
+            qu.name,
+            qu.questiontext,
+            qua.rightanswer,
+            qua.responsesummary as StudentAns,
+            qua.maxmark,
+            qc.name as category
+
+        FROM
+            mdl_quiz q,
+            mdl_quiz_slots qs,
+            mdl_question qu,
+            mdl_question_categories qc,
+            mdl_quiz_attempts qa,
+            mdl_question_attempts qua
+
+        WHERE
+            q.id=qs.quizid AND qu.id=qs.questionid AND qu.category=qc.id AND q.id=qa.quiz
+            AND qa.uniqueid=qua.questionusageid AND qu.id=qua.questionid
+
+        ORDER BY qa.attempt, qa.userid
+        */
+
+        ?>
+
+        <?php
+        echo $OUTPUT->footer();
+    }
+    else
+    {?>
+        <h2 style="color:red;"> Invalid Selection </h2>
+        <a href="./display_courses.php">Back</a>
+    <?php 
+        echo $OUTPUT->footer();
+    }?>
---
+++ display_quizzes.php
@@ -0,0 +1,70 @@
+<script src="./script/jquery/jquery-3.2.1.js"></script>
+
+<?php 
+    require_once('../config.php');
+    $context = context_system::instance();
+    $PAGE->set_context($context);
+    $PAGE->set_pagelayout('admin');
+    $PAGE->set_title("My Quizzes");
+    $PAGE->set_heading("Quizzes");
+    $PAGE->set_url($CFG->wwwroot.'/custom/display_quizzes.php');
+    
+    echo $OUTPUT->header();
+    require_login();
+
+    if(isset($_POST['submit']) && isset( $_POST['courseid']))
+    {
+        $course_id=$_POST['courseid'];
+        //echo "Course ID : $course_id";
+    
+        // Dispaly all quizzes
+        //$rec=$DB->get_records_sql('SELECT * FROM  `mdl_quiz` WHERE course = ? AND timeopen != ?', array($course_id, 0));
+        $rec=$DB->get_records_sql('SELECT * FROM  `mdl_quiz` WHERE course = ? AND id IN (SELECT quiz FROM `mdl_quiz_attempts`)', array($course_id));
+        if($rec){
+            ?>
+            <form method='post' action='display_quiz_grid.php' id="form_check">
+            <?php
+            $serialno = 0;
+            $table = new html_table();
+            $table->head = array('S. No.', 'Name', 'Intro', 'Select');
+            foreach ($rec as $records) {
+                $serialno++;
+                $id = $records->id;
+                $courseid = $records->course;
+                $name = $records->name;
+                $intro = $records->intro;
+                $table->data[] = array($serialno, $name, $intro, '<input type="radio" value="'.$id.'" name="quizid">');
+            }
+            echo html_writer::table($table);
+            ?>
+            <input type="submit" value="NEXT" name="submit" class="btn btn-primary">
+            </form>
+            <br />
+            <p id="msg"></p>
+
+            <script>
+            $('#form_check').on('submit', function (e) {
+                if ($("input[type=radio]:checked").length === 0) {
+                    e.preventDefault();
+                    $("#msg").html("<font color='red'>Select any one quiz!</font>");
+                    return false;
+                }
+            });
+            </script>
+
+            <?php
+            echo $OUTPUT->footer();
+        }
+        else{
+            echo "<h3>No quizzes found!</h3>";
+            echo $OUTPUT->footer();
+        }
+
+    }
+    else
+    {?>
+        <h2 style="color:red;"> Invalid Selection </h2>
+        <a href="./display_courses.php">Back</a>
+    <?php
+        echo $OUTPUT->footer();
+    }?>
