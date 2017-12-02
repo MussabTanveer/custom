098de3d3c69b9957dcd46e7b6428fc53ae73f9af
Syedkamran1 <syed_mkamran@outlook.com>
2017-12-03 00:57:01 +0500

    Mapped Clos to Course

---
+++ map_clo_course.php
@@ -0,0 +1,106 @@
+<?php
+	require_once('../config.php');
+    $context = context_system::instance();
+    $PAGE->set_context($context);
+    $PAGE->set_pagelayout('admin');
+    $PAGE->set_title("OBE Frameworks");
+    $PAGE->set_heading("OBE Framework Selection");
+    $PAGE->set_url($CFG->wwwroot.'/custom/select_framework_to_map_clo_course.php');
+    
+    echo $OUTPUT->header();
+    require_login();
+    is_siteadmin() || die('<h2>This page is for site admins only!</h2>'.$OUTPUT->footer());
+    global $SESSION;
+    $time = time();
+    
+ 
+ if((isset($_POST['submit']) && isset($_POST['frameworkid']))  || isset($SESSION->fid6))
+    {
+
+    	if(isset($SESSION->fid6) && $SESSION->fid6 != "xyz")
+		 {
+		 	$frameworkid=$SESSION->fid6; 
+		 	$SESSION->fid6="xyz";
+		 }
+		else
+   		 $frameworkid=$_POST['frameworkid'];
+    		//echo "$frameworkid";
+
+    		
+    
+ 		 $courseid=$SESSION->courseid;
+ 		//echo "$courseid";
+
+ 		 $course=$DB->get_records_sql('SELECT * FROM `mdl_course` 
+    		WHERE id = ? ',
+    		 array($courseid));
+
+ 		 if ($course != NULL){
+ 		 	foreach ($course as $rec) {
+							$id =  $rec->id;
+							$idnumber =  $rec->idnumber;
+
+
+							}
+ 		 }	
+
+ 		 $count=0;
+//echo "$frameworkid";
+ 		 //echo "$idnumber";
+
+		$competencies=$DB->get_records_sql("SELECT * FROM `mdl_competency` 
+    		WHERE idnumber like '{$idnumber}%' 
+    		AND competencyframeworkid =? ",
+    		 array($frameworkid));
+
+	$flag=false;
+
+ if ($competencies != NULL){
+ 		 	foreach ($competencies as $rec) {
+							$id =  $rec->id;
+							$idnumber =  $rec->idnumber;
+							//echo "$idnumber";
+
+					$check=$DB->get_records_sql("SELECT * FROM `mdl_competency_coursecomp` 
+					    		WHERE courseid = ? 
+					    		AND competencyid =? ",
+					    		 array($courseid,$id));
+
+							if ($check == NULL)
+							{	
+								$flag=true;
+							
+								$sql="INSERT INTO mdl_competency_coursecomp (courseid, competencyid,ruleoutcome,timecreated,timemodified,usermodified,sortorder) VALUES ('$courseid', '$id','1','$time','$time', '$USER->id','0')";
+								$DB->execute($sql);
+								
+							}
+
+
+							}
+
+							if($flag == true)
+							{
+								echo " <font color='green'>CLOs successfully mapped with the course </font>";
+							}
+			
+
+ 		 }
+
+ 		 else 
+ 		 {	echo " <font color='red'>No CLOs of this course have been added to the framework</font>";
+
+ 			 goto end;
+ 			}
+
+
+ 		 if ($flag == false)
+ 		{
+ 			echo " <font color='green'>CLOs are already mapped with the course </font>";
+ 		 }
+ 		
+		end:
+
+ 	}
+?>
+<br> </br>
+ 	<a href="./select_course.php">Back</a>
---
+++ select_course.php
@@ -0,0 +1,57 @@
+<script src="./script/jquery/jquery-3.2.1.js"></script>
+<?php
+    require_once('../config.php');
+    $context = context_system::instance();
+    $PAGE->set_context($context);
+    $PAGE->set_pagelayout('admin');
+    $PAGE->set_title("Select Course");
+    $PAGE->set_heading("Select Course");
+    $PAGE->set_url($CFG->wwwroot.'/custom/select_course.php');
+    
+    echo $OUTPUT->header();
+	require_login();
+    is_siteadmin() || die('<h2>This page is for site admins only!</h2>'.$OUTPUT->footer());
+	
+	$courses=$DB->get_records_sql('SELECT * FROM `mdl_course` WHERE id != ?', array(1));
+    $i=0;
+    if($courses){
+        ?>
+        <form method="post" action="select_framework_to_map_clo_course.php" id="form_check">
+        <?php
+        $table = new html_table();
+        $table->head = array('S.No','Course Name' ,'ID Number','Select');
+        foreach ($courses as $records){
+            $i++;
+            $fullname = $records->fullname;
+            $shortname = $records->shortname;
+            $idnumber = $records->idnumber;
+            $id=$records->id;
+            $table->data[] = array( $i,$fullname,$idnumber,'<input type="radio" value="'.$id.'" name="courseid">'); 
+            
+        }
+        echo html_writer::table($table);
+
+        ?>
+        <input type='submit' value='NEXT' name='submit' class="btn btn-primary">
+        </form>
+       
+        <br />
+        <p id="msg"></p>
+        
+        <script>
+        $('#form_check').on('submit', function (e) {
+            if ($("input[type=radio]:checked").length === 0) {
+                e.preventDefault();
+                $("#msg").html("<font color='red'>Please select a Course!</font>");
+                return false;
+            }
+        });
+        </script>
+
+        <?php
+    }
+    else{
+        echo "<h3>No courses found!</h3>";
+    }
+    echo $OUTPUT->footer();
+	?>
