64bdcff7a243462a9e7c706d10ca5f59865ca5de
MussabTanveer <mussab.tanveer9@gmail.com>
2018-01-13 16:47:24 +0500

    display courses of particular teacher

--- chairman/display_courses-3.php
+++ chairman/display_courses-3.php
@@ -12,57 +12,77 @@
     echo $OUTPUT->header();
     require_login();
 
-    // Dispaly all courses
-    //$rec=$DB->get_records_sql('SELECT * FROM  `mdl_course` WHERE startdate != ? AND visible = ?', array( 0 , 1 ));
-    $rec=$DB->get_records_sql('SELECT c.id, c.fullname, c.shortname, c.idnumber
-    
-        FROM mdl_course c, mdl_user us, mdl_role r, mdl_role_assignments ra
-    
-        WHERE us.id=ra.userid AND r.id=ra.roleid AND  r.shortname=? AND us.id=? AND c.id != ?', array('chairman',$USER->id,1));
+    if(isset($_GET['tid']))
+	{
+        $tid=$_GET['tid'];
 
-    if($rec){
-        ?>
-        <form method="post" action="display_activities.php" id="form_check">
-        <?php
-        $serialno = 0;
-        $table = new html_table();
-        $table->head = array('S. No.','Full Name', 'Short Name' , 'Course Code', 'Select');
-        foreach ($rec as $records) {
-            $serialno++;
-            $id = $records->id;
-            $fname = $records->fullname;
-            $sname = $records->shortname;
-            $idnum = $records->idnumber;
-            $table->data[] = array($serialno, $fname, $sname, $idnum, '<input type="radio" value="'.$id.'" name="courseid">');
-        }
-        if($serialno == 1){
-            
-            global $SESSION;
-            $SESSION->cid3 = $id;
-        
-            redirect('display_activities.php');
-        }
-        echo html_writer::table($table);
-        ?>
-        <input type='submit' value='NEXT' name='submit' class="btn btn-primary">
-        </form>
-        <br />
-        <p id="msg"></p>
+        // Dispaly all courses
+        $rec=$DB->get_records_sql('SELECT c.id, c.fullname, c.shortname, c.idnumber
+    
+        FROM mdl_course c
+    
+        INNER JOIN mdl_context cx ON c.id = cx.instanceid
+    
+        AND cx.contextlevel = ?
+    
+        INNER JOIN mdl_role_assignments ra ON cx.id = ra.contextid
+    
+        INNER JOIN mdl_role r ON ra.roleid = r.id
+    
+        INNER JOIN mdl_user usr ON ra.userid = usr.id
+    
+        WHERE r.shortname = ?
+    
+        AND usr.id = ?', array('50', 'editingteacher', $tid));
 
-        <script>
-        $('#form_check').on('submit', function (e) {
-            if ($("input[type=radio]:checked").length === 0) {
-                e.preventDefault();
-                $("#msg").html("<font color='red'>Select any one course!</font>");
-                return false;
+        if($rec){
+            ?>
+            <form method="post" action="display_activities.php" id="form_check">
+            <?php
+            $serialno = 0;
+            $table = new html_table();
+            $table->head = array('S. No.','Full Name', 'Short Name' , 'Course Code', 'Select');
+            foreach ($rec as $records) {
+                $serialno++;
+                $id = $records->id;
+                $fname = $records->fullname;
+                $sname = $records->shortname;
+                $idnum = $records->idnumber;
+                $table->data[] = array($serialno, $fname, $sname, $idnum, '<input type="radio" value="'.$id.'" name="courseid">');
             }
-        });
-        </script>
-        <?php
-        echo $OUTPUT->footer();
+            if($serialno == 1){
+                
+                global $SESSION;
+                $SESSION->cid3 = $id;
+            
+                redirect('display_activities.php');
+            }
+            echo html_writer::table($table);
+            ?>
+            <input type='submit' value='NEXT' name='submit' class="btn btn-primary">
+            </form>
+            <br />
+            <p id="msg"></p>
+
+            <script>
+            $('#form_check').on('submit', function (e) {
+                if ($("input[type=radio]:checked").length === 0) {
+                    e.preventDefault();
+                    $("#msg").html("<font color='red'>Select any one course!</font>");
+                    return false;
+                }
+            });
+            </script>
+            <?php
+            echo $OUTPUT->footer();
+        }
+        else{
+            echo "<h3>No courses of this teacher found!</h3>";
+            echo $OUTPUT->footer();
+        }
     }
     else{
-        echo "<h3>No courses found!</h3>";
+        echo "<h3>Invalid Selection!</h3>";
         echo $OUTPUT->footer();
     }
 ?>
