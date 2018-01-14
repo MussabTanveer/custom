b8ad16048a8b9e044e04f5f70dd3aeab0542c0fd
MussabTanveer <mussab.tanveer9@gmail.com>
2018-01-14 16:55:16 +0500

    save vision & misssion

---
+++ chairman/save_vision_mission.php
@@ -0,0 +1,40 @@
+<?php
+	require_once('../../../config.php');
+    $context = context_system::instance();
+    $PAGE->set_context($context);
+    $PAGE->set_pagelayout('standard');
+    $PAGE->set_title("Vision & Mission");
+    $PAGE->set_heading("Define Vision & Mission");
+    $PAGE->set_url($CFG->wwwroot.'/local/ned_obe/admin/save_vision_mission.php');
+    
+	$universityVision = trim($_POST["uv"]);
+	$universityMission = trim($_POST["um"]);
+	$departmentVision = trim($_POST["dv"]);
+	$departmentMission = trim($_POST["dm"]);
+
+
+	/*echo "$universityVision <br>";
+	echo "$universityMission <br>";
+	echo "$departmentVision <br>";
+	echo "$departmentMission <br> ";*/
+
+	$universityVision = mysql_real_escape_string($universityVision);
+	$universityMission = mysql_real_escape_string($universityMission);
+	$departmentVision = mysql_real_escape_string($departmentVision);
+	$departmentMission = mysql_real_escape_string($departmentMission);
+
+	
+	$sql="UPDATE `mdl_vision_mission` SET description = '$universityVision' WHERE idnumber='uv'";
+	$DB->execute($sql);
+
+	$sql="UPDATE `mdl_vision_mission` SET description = '$universityMission' WHERE idnumber='um'";
+	$DB->execute($sql);
+
+	$sql="UPDATE `mdl_vision_mission` SET description = '$departmentVision' WHERE idnumber='dv'";
+	$DB->execute($sql);
+
+	$sql="UPDATE `mdl_vision_mission` SET description = '$departmentMission' WHERE idnumber='dm'";
+		
+	$DB->execute($sql);
+
+?>
