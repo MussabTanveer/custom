6fd825e9861371b2fecc2899a79299c3a44536d4
MussabTanveer <mussab.tanveer9@gmail.com>
2018-01-14 17:13:26 +0500

    chairman map clo to level

---
+++ chairman/display_outcome_framework-3.php
@@ -0,0 +1,60 @@
+<script src="../script/jquery/jquery-3.2.1.js"></script>
+
+<?php
+    require_once('../../../config.php');
+    $context = context_system::instance();
+    $PAGE->set_context($context);
+    $PAGE->set_pagelayout('standard');
+    $PAGE->set_title("OBE Frameworks");
+    $PAGE->set_heading("OBE Framework Selection");
+    $PAGE->set_url($CFG->wwwroot.'/local/ned_obe/chairman/display_outcome_framework-3.php');
+    
+    echo $OUTPUT->header();
+    require_login();
+    
+    // Dispaly all frameworks
+    $rec=$DB->get_records_sql('SELECT * FROM mdl_competency_framework WHERE mdl_competency_framework.id NOT IN (SELECT mdl_taxonomy_clo_level.frameworkid FROM mdl_taxonomy_clo_level)');
+    if($rec){
+        ?>
+        <form method="post" action="map_clo_level.php" id="form_check">
+        <?php
+        $serialno = 0;
+        $table = new html_table();
+        $table->head = array('S. No.','Framework', 'Select');
+        foreach ($rec as $records) {
+            $serialno++;
+            $id = $records->id;
+            $sname = $records->shortname;
+            $table->data[] = array($serialno, $sname, '<input type="radio" value="'.$id.'" name="fwid">');
+        }
+        if($serialno == 1){
+            
+            global $SESSION;
+            $SESSION->fid9 = $id;
+        
+            redirect('map_clo_level.php');
+        }
+        echo html_writer::table($table);
+        ?>
+        <input type='submit' value='NEXT' name='submit' class="btn btn-primary">
+        </form>
+        <br />
+        <p id="msg"></p>
+
+        <script>
+        $('#form_check').on('submit', function (e) {
+            if ($("input[type=radio]:checked").length === 0) {
+                e.preventDefault();
+                $("#msg").html("<font color='red'>Select any one framework!</font>");
+                return false;
+            }
+        });
+        </script>
+        <?php
+        echo $OUTPUT->footer();
+    }
+    else{
+        echo "<h3>No framework found!</h3>";
+        echo $OUTPUT->footer();
+    }
+?>
