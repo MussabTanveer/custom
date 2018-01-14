07a8c95eea985129fdb5ac8aea56bf40071b8ef7
MussabTanveer <mussab.tanveer9@gmail.com>
2018-01-14 17:11:39 +0500

    chairman map plo to domain

---
+++ chairman/confirm_domain_plo.php
@@ -0,0 +1,105 @@
+<?php
+	require_once('../../../config.php');
+    $context = context_system::instance();
+	$PAGE->set_context($context);
+    $PAGE->set_pagelayout('standard');
+    $PAGE->set_title("Add Domain");
+    $PAGE->set_heading("Add PLO Domain");
+    $PAGE->set_url($CFG->wwwroot.'/local/ned_obe/chairman/confirm_domain_plo.php');
+    
+	echo $OUTPUT->header();
+	require_login();
+     
+	if(isset($_POST['submit']))
+	{
+		$plocount = $_POST['plocount'];
+		$fw_id = $_POST['fwid'];
+		//echo $fw_id;
+
+		$cognitivearray = array();
+		for ($i = 0; $i < $plocount; $i++) {
+			$cognitivearray[$i] = in_array($i, $_POST['cognitive']) ? 1 : 0;
+		}
+
+		$psychomotorarray = array();
+		for ($i = 0; $i < $plocount; $i++) {
+			$psychomotorarray[$i] = in_array($i, $_POST['psychomotor']) ? 1 : 0;
+		}
+
+		$affectivearray = array();
+		for ($i = 0; $i < $plocount; $i++) {
+			$affectivearray[$i] = in_array($i, $_POST['affective']) ? 1 : 0;
+		}
+	}
+	$ploidsarray=$SESSION->ploids;
+	/*
+	echo $ploidsarray[0];
+	echo "<br>";
+	echo $ploidsarray[1];
+	echo "<br>";
+	echo $ploidsarray[2];
+	echo "<br>";
+	echo $ploidsarray[3];
+	echo "<br>";
+	echo $ploidsarray[4];
+	echo "<br>";echo "<br>";
+	
+	echo $cognitivearray[0];
+	echo "<br>";
+	echo $cognitivearray[1];
+	echo "<br>";
+	echo $cognitivearray[2];
+	echo "<br>";
+	echo $cognitivearray[3];
+	echo "<br>";
+	echo $cognitivearray[4];
+	echo "<br>";echo "<br>";
+
+	echo $psychomotorarray[0];
+	echo "<br>";
+	echo $psychomotorarray[1];
+	echo "<br>";
+	echo $psychomotorarray[2];
+	echo "<br>";
+	echo $psychomotorarray[3];
+	echo "<br>";
+	echo $psychomotorarray[4];
+	echo "<br>";echo "<br>";
+
+	echo $affectivearray[0];
+	echo "<br>";
+	echo $affectivearray[1];
+	echo "<br>";
+	echo $affectivearray[2];
+	echo "<br>";
+	echo $affectivearray[3];
+	echo "<br>";
+	echo $affectivearray[4];
+	echo "<br>";
+	*/
+	$i=0;
+	foreach ($ploidsarray as $pid)
+	{
+		if($cognitivearray[$i] != 0){
+			$sql="INSERT INTO mdl_taxonomy_plo_domain (frameworkid, ploid, domainid) VALUES($fw_id, $pid, 1)";
+			$DB->execute($sql);
+		}
+		if($psychomotorarray[$i] != 0){
+			$sql="INSERT INTO mdl_taxonomy_plo_domain (frameworkid, ploid, domainid) VALUES($fw_id, $pid, 2)";
+			$DB->execute($sql);
+		}
+		if($affectivearray[$i] != 0){
+			$sql="INSERT INTO mdl_taxonomy_plo_domain (frameworkid, ploid, domainid) VALUES($fw_id, $pid, 3)";
+			$DB->execute($sql);
+		}
+		$i++;
+	}
+	
+	echo "<font color='green'>Domains successfully mapped with the PLOs!</font>";
+	echo "<br>";
+?>
+<a href="./display_outcome_framework-2.php">Back</a>
+
+<?php 	
+	echo $OUTPUT->footer();
+?>
---
+++ chairman/map_plo_domain.php
@@ -0,0 +1,71 @@
+<?php 
+    require_once('../../../config.php');
+    $context = context_system::instance();
+    $PAGE->set_context($context);
+    $PAGE->set_pagelayout('standard');
+    $PAGE->set_title("PLOs and Domains");
+    $PAGE->set_heading("Map PLOs to Taxonomy Domains");
+    $PAGE->set_url($CFG->wwwroot.'/local/ned_obe/chairman/map_plo_domain.php');
+    
+    echo $OUTPUT->header();
+    require_login();
+    
+    if((isset($_POST['submit']) && isset( $_POST['fwid'])) || (isset($SESSION->fid8) && $SESSION->fid8 != "xyz"))
+    {
+        if(isset($SESSION->fid8) && $SESSION->fid8 != "xyz")
+        {
+            $fw_id=$SESSION->fid8;
+            $SESSION->fid8 = "xyz";
+        }
+        else
+            $fw_id=$_POST['fwid'];
+        //echo "FW ID : $fw_id";
+    ?>
+
+        <?php
+        // Get PLOs
+        $rec=$DB->get_records_sql("SELECT * FROM mdl_competency WHERE competencyframeworkid = ? AND idnumber LIKE 'PLO%'  ORDER BY id", array($fw_id));
+
+        if($rec){
+            $i = 0;
+            ?>
+            <form action="confirm_domain_plo.php" method="post" >
+            <?php
+            $ploids = array();
+            $table = new html_table();
+            $table->head = array('PLOs','COGNITIVE', 'PSYCHOMOTOR', 'AFFECTIVE');
+            foreach ($rec as $records) {
+                $id = $records->id;
+                $sname = $records->shortname;
+                $table->data[] = array($sname, '<input type="checkbox" value="'.$i.'" name="cognitive[]">', '<input type="checkbox" value="'.$i.'" name="psychomotor[]">', '<input type="checkbox" value="'.$i.'" name="affective[]">');
+                array_push($ploids, $id);
+                $i++;
+            }
+            echo html_writer::table($table);
+            global $SESSION;
+            $SESSION->ploids = $ploids;
+            ?>
+                <input type="hidden" value='<?php echo $i; ?>' name="plocount">
+                <input type="hidden" value='<?php echo $fw_id; ?>' name="fwid">
+                <input type="submit" value="NEXT" name="submit" class="btn btn-primary">
+	    	</form>
+            
+            <a href="./display_outcome_framework-2.php">Back</a>
+
+            <?php
+            echo $OUTPUT->footer();
+        }
+        else{
+            echo "<h3>No PLOs found!</h3>";
+            echo '<a href="./display_outcome_framework-2.php">Back</a>';
+            echo $OUTPUT->footer();
+        }
+
+    }
+    else
+    {?>
+        <h2 style="color:red;"> Invalid Selection </h2>
+        <a href="./display_outcome_framework-2.php">Back</a>
+    <?php
+        echo $OUTPUT->footer();
+    }?>
