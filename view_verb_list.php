f3b52174639e2ea3ed757f843caaa7470c6ed58d
MussabTanveer <mussab.tanveer9@gmail.com>
2018-01-24 03:05:47 +0500

    move view verb list file from teacher to main folder and allow chairman to view it

--- view_verb_list.php
+++ view_verb_list.php
@@ -1,15 +1,15 @@
 <?php
-    require_once('../../../config.php');
+    require_once('../../config.php');
     $context = context_system::instance();
     $PAGE->set_context($context);
     $PAGE->set_pagelayout('standard');
     $PAGE->set_title("View Verb List");
     $PAGE->set_heading("View Verb List");
-    $PAGE->set_url($CFG->wwwroot.'/local/ned_obe/teacher/view_verb_list.php');
+    $PAGE->set_url($CFG->wwwroot.'/local/ned_obe/view_verb_list.php');
    
     require_login();
 
-  global $CFG;
+    global $CFG;
     $x= $CFG->dbpass;
 
 class Blob{
@@ -40,9 +40,9 @@ class Blob{
 public function selectBlob($id) {
  
         $sql = "SELECT mime,
-                        data
-                   FROM mdl_verb_list
-                  WHERE id = :id;";
+        data
+        FROM mdl_verb_list
+        WHERE id = :id;";
  
         $stmt = $this->pdo->prepare($sql);
         $stmt->execute(array(":id" => $id));
@@ -61,54 +61,44 @@ public function selectBlob($id) {
         // close the database connection
         $this->pdo = null;
     }
-
 }
 
-$query=$DB->get_records_sql('SELECT DISTINCT r.shortname FROM  mdl_role r , 
+    $query=$DB->get_records_sql('SELECT DISTINCT r.shortname FROM  mdl_role r,
+    mdl_role_assignments ra 
+    WHERE (ra.userid= ? AND ra.roleid=r.id AND r.shortname= ?)', array($USER->id, 'editingteacher'));
 
-          mdl_role_assignments ra 
-         WHERE (ra.userid= ? AND ra.roleid=r.id AND r.shortname= ?)', array($USER->id, 'editingteacher'));
+    $query2=$DB->get_records_sql('SELECT us.username FROM mdl_user us, mdl_role r,mdl_role_assignments ra   WHERE us.id=ra.userid AND r.id=ra.roleid AND  r.shortname=? AND us.id=? ',array('chairman',$USER->id));
 
-        if($query || is_siteadmin())
+    if($query || $query2 || is_siteadmin())
+    {
+        echo "Teacher";
+    
+        $recs=$DB->get_records_sql('SELECT * FROM mdl_verb_list');
+
+        if($recs)
         {
-            echo "Teacher";
-        
-        
-
-
-
-		 $recs=$DB->get_records_sql('SELECT * FROM mdl_verb_list');
-
-		 if($recs)
-		 {
-		 	foreach ($recs as $rec)
-		 	{
-				$id =  $rec->id;
-				
-			}
-        
-		}
-		else
+            foreach ($recs as $rec)
+            {
+                $id =  $rec->id;
+            }
+        }
+        else
         {
             echo $OUTPUT->header();
-			echo "<font size =8, color=red>Verb List hasn't uploaded yet!</font>";
-            
+            echo "<font size=5, color=red>Verb list hasn't been uploaded yet!</font>";
             echo $OUTPUT->footer();
         }
 		
-	
-
-                //displaying pdf
-            $blobObj = new Blob($x);
-            $a = $blobObj->selectBlob($id);
-            header("Content-Type:" . $a['mime']);
-              echo $a['data'];
+        //displaying pdf
+        $blobObj = new Blob($x);
+        $a = $blobObj->selectBlob($id);
+        header("Content-Type:" . $a['mime']);
+        echo $a['data'];
     }
     else
-    {           
-        echo $OUTPUT->header();
-            
-            echo "<font size =5> This page is for Admin OR Teachers Only!</font>";
-             echo $OUTPUT->footer();
-
-        }
+    {
+        echo $OUTPUT->header();    
+        echo "<font size=5> You do not have permission to view this page!</font>";
+        echo $OUTPUT->footer();
+    }
+    
