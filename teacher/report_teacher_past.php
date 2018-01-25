907e49a1039c9b271fa8caa98294106c5f06a8ee
MussabTanveer <mussab.tanveer9@gmail.com>
2018-01-25 21:10:28 +0500

    past course reports can be viewed any chnages not allowed

---
+++ teacher/report_teacher_past.php
@@ -0,0 +1,55 @@
+<script src="../script/jquery/jquery-3.2.1.js"></script>
+
+<?php
+    require_once('../../../config.php');
+    $context = context_system::instance();
+    $PAGE->set_context($context);
+    $PAGE->set_pagelayout('standard');
+    $PAGE->set_title("Teacher Reports");
+    $PAGE->set_heading("Teacher Reports");
+    $PAGE->set_url($CFG->wwwroot.'/local/ned_obe/teacher/report_teacher_past.php');
+    echo $OUTPUT->header();
+    require_login();
+
+    if(isset($_GET['course']))
+    {
+        $course_id=$_GET['course'];
+    ?>
+    <link rel="stylesheet" type="text/css" href="../css/cool-link/style.css" />
+
+	<div>
+    <h5 style="color: green"> Note: You can only view past course profile, grading policy and reports. Any changes in past course are strictly prohibited. </h5><br>
+        <h3> Click the links down below as per need </h3><br>
+
+        <a <?php echo "href='./view_course_profile.php?course=$course_id'" ?> class="cool-link">View Course Profile</a><br><br>
+
+        <a <?php echo "href='./display_grading_policy.php?course=$course_id'" ?>  class="cool-link">Display Grading Policy</a><br><br>
+        
+        <a href="javascript:void(0)" onclick="toggle_visibility('clorep');" class="cool-link">CLO Reports</a><br><br>
+        <div id="clorep" style="display: none">
+            &nbsp;&nbsp;&nbsp;<a <?php echo "href='./display_quizzes.php?course=$course_id'" ?> class="cool-link">&#10070; View Activity Detailed Report</a><br>
+            &nbsp;&nbsp;&nbsp;<a <?php echo "href='./display_activities.php?course=$course_id'" ?> class="cool-link">&#10070; Activity/Course CLO Report</a><br>
+            &nbsp;&nbsp;&nbsp;<a <?php echo "href='./course_report.php?course=$course_id'" ?> class="cool-link">&#10070; Course Report</a><br><br>
+        </div>
+        
+    </div>
+
+    <script type="text/javascript">
+        function toggle_visibility(id) {
+        var e = document.getElementById(id);
+        if(e.style.display == 'block')
+            e.style.display = 'none';
+        else
+            e.style.display = 'block';
+        }
+    </script>
+    <?php
+    }
+    else
+    {?>
+        <h2 style="color:red;"> Invalid Selection </h2>
+        <a href="./teacher_courses.php">Back</a>
+    <?php
+    }
+    echo $OUTPUT->footer();
+?>
