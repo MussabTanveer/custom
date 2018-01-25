3b17f71fc16baf49073cfe60e9ba257a59de1ac3
MussabTanveer <mussab.tanveer9@gmail.com>
2018-01-26 01:54:06 +0500

    check session for non-editing teacher role

--- noneditingteacher/report_noneditingteacher.php
+++ noneditingteacher/report_noneditingteacher.php
@@ -6,10 +6,12 @@
     $PAGE->set_title("Non-Editing Teacher");
     $PAGE->set_heading("Non-Editing Teacher Courses");
     $PAGE->set_url($CFG->wwwroot.'/local/ned_obe/chairman/report_noneditingteacher.php');
-    echo $OUTPUT->header();
+    
     require_login();
-    //$rec1=$DB->get_records_sql('SELECT us.username FROM mdl_user us, mdl_role r,mdl_role_assignments ra   WHERE us.id=ra.userid AND r.id=ra.roleid AND  r.shortname=? AND us.id=? ',array('chairman',$USER->id));
-    //$rec1 || die('<h2>This page is for Chairperson only!</h2>'.$OUTPUT->footer());
+    if($SESSION->oberole != "noneditingteacher"){
+        header('Location: ../index.php');
+    }
+    echo $OUTPUT->header();
 
 
     $rec=$DB->get_records_sql('SELECT c.id, c.fullname , c.shortname, c.idnumber
