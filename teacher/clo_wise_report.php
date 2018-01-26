6508b38efc5eb569450c0fb3f447cd3f701cc8e7
MussabTanveer <mussab.tanveer9@gmail.com>
2018-01-26 23:50:15 +0500

    query to get course students and clos

--- teacher/clo_wise_report.php
+++ teacher/clo_wise_report.php
@@ -13,10 +13,62 @@
 	}
 	echo $OUTPUT->header();
 
+    if(isset($_GET['course']))
+    {
+        $course_id=$_GET['course'];
+        
+        // Get all students of course
+        $recStudents=$DB->get_records_sql("SELECT u.id AS sid, u.username AS seatnum, u.firstname, u.lastname
+        FROM mdl_role_assignments ra, mdl_user u, mdl_course c, mdl_context cxt
+        WHERE ra.userid = u.id
+        AND ra.contextid = cxt.id
+        AND cxt.contextlevel = ?
+        AND cxt.instanceid = c.id
+        AND c.id = ?
+        AND (roleid=5)", array(50, $course_id));
 
+        $stdids = array();
+        $seatnos = array();
+        foreach($recStudents as $records){
+            $id = $records->sid;
+            $seatno = $records->seatnum ;
+            array_push($stdids,$id);
+            array_push($seatnos,$seatno);
+
+        }
+
+        //Get course clo with its level, plo and peo
+		$courseclos=$DB->get_records_sql(
+        "SELECT clo.id AS cloid, clo.shortname AS cloname, plo.shortname AS ploname, peo.shortname AS peoname, levels.name AS lname, levels.level AS lvl
+    
+        FROM mdl_competency_coursecomp cc, mdl_competency clo, mdl_competency plo, mdl_competency peo, mdl_taxonomy_levels levels, mdl_taxonomy_clo_level clolevel
+
+        WHERE cc.courseid = ? AND cc.competencyid=clo.id  AND peo.id=plo.parentid AND plo.id=clo.parentid AND 
+        clo.id=clolevel.cloid AND levels.id=clolevel.levelid",
+        
+        array($course_id));
+            
+        $clonames = array(); $closid = array(); $plos = array(); $peos = array(); $levels = array(); $lvlno = array();
+        foreach ($courseclos as $recC) {
+            $cid = $recC->cloid;
+            $clo = $recC->cloname;
+            $plo = $recC->ploname;
+            $peo = $recC->peoname;
+            $lname = $recC->lname;
+            $lvl = $recC->lvl;
+            array_push($closid, $cid); // array of clo ids
+            array_push($clonames, $clo); // array of clo names
+            array_push($plos, $plo); // array of plos
+            array_push($peos, $peo); // array of peos
+            array_push($levels, $lname); // array of levels
+            array_push($lvlno, $lvl); // array of level nos
+        }
+
+    }
     
 
 
 
-     echo $OUTPUT->footer();
+    echo $OUTPUT->footer();
 
+?>
