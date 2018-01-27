aa91947c961ba69a5f751388aa4995cfe0e9802b
MussabTanveer <mussab.tanveer9@gmail.com>
2018-01-27 23:35:52 +0500

    filter student records form

--- chairman/display_students.php
+++ chairman/display_students.php
@@ -14,6 +14,51 @@
     $rec1=$DB->get_records_sql('SELECT us.username FROM mdl_user us, mdl_role r,mdl_role_assignments ra   WHERE us.id=ra.userid AND r.id=ra.roleid AND  r.shortname=? AND us.id=? ',array('chairman',$USER->id));
     $rec1 || die('<h2>This page is for Chairperson only!</h2>'.$OUTPUT->footer());
 
+    if(isset($_POST['inputtext'])){
+        $nam=$_POST['inputtext'];
+    }
+    //query to get all cohorts    
+    $rec2=$DB->get_records_sql('SELECT c.name from mdl_cohort as c');
+?>
+
+    <form action="action_file.php" class="form-inline">
+        <h5 autofocus style="text-align:center;">Filter Records By</h5>
+        <p style="text-align:center;">
+        <select autofocus id="Filtercheck" name="dropdown" onChange="cohortdropdown()" class="select custom-select">
+        <option value="seatnumber">Seat number</option>
+        <option value="batchnumber">Batch number</option>
+        <option value="name">Name</option>
+        <option value="cohort">Cohort</option>
+        </select>
+        <select style="display:none"  id="cohort_dd" name="cohort_dd" class="select custom-select">
+        <?php
+        foreach ($rec2 as $records2) {
+            $name =  $records2->name;					
+        ?>
+        <option value='<?php echo $name; ?>'><?php echo $name; ?></option>
+        <?php
+        }
+        ?>
+        <input id="inputcheck" name="inputtext" type="text" placeholder="Enter Value" class="form-control">
+        <input id="buttoncheck" type="submit" class="btn btn-info">
+        </p>
+    </form>
+    <br />
+    <script>
+    function cohortdropdown(){
+        var a=document.getElementById('Filtercheck').value;
+        if (a=='cohort'){
+            document.getElementById('inputcheck').style.display="none";
+            document.getElementById('cohort_dd').style.display="inline";
+        }
+        else{
+            document.getElementById('inputcheck').style.display="inline";
+            document.getElementById('cohort_dd').style.display="none";
+        }
+    }
+    </script>
+
+<?php
     //query to show all students
     $rec=$DB->get_records_sql('SELECT distinct u.id, u.username, CONCAT( u.firstname, " ", u.lastname ) AS student , u.idnumber FROM mdl_course as c, mdl_role_assignments AS ra, mdl_user AS u, mdl_context AS ct WHERE c.id = ct.instanceid AND ra.roleid =5 AND ra.userid = u.id AND ct.id = ra.contextid ORDER BY u.idnumber');
     
