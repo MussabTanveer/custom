a666f0f95fee10a4dd7c941fccd8247b7fe4f61d
MussabTanveer <mussab.tanveer9@gmail.com>
2018-02-05 01:09:17 +0500

    print empty grading sheet for assign/project

---
+++ teacher/export2.php
@@ -0,0 +1,59 @@
+<?php 
+    require_once('../../../config.php');
+    require_once('../script/spout-2.4.3/spout-2.4.3/src/Spout/Autoloader/autoload.php');
+    $PAGE->set_url($CFG->wwwroot.'/local/ned_obe/teacher/print_assignment_grading_sheet.php');
+    use Box\Spout\Writer\WriterFactory;
+    use Box\Spout\Common\Type;
+    require_login();
+    if($SESSION->oberole != "teacher"){
+        header('Location: ../index.php');
+    }
+
+    if(!empty($_GET['id']) && !empty($_GET['course']))
+    {
+        $course_id=$_GET['course'];
+        $quiz_id=$_GET['id'];
+
+        // Get all students of course
+        $recStudents=$DB->get_records_sql("SELECT u.id AS sid, u.username AS seatnum, u.firstname, u.lastname
+        FROM mdl_role_assignments ra, mdl_user u, mdl_course c, mdl_context cxt
+        WHERE ra.userid = u.id
+        AND ra.contextid = cxt.id
+        AND cxt.contextlevel = ?
+        AND cxt.instanceid = c.id
+        AND c.id = ?
+        AND (roleid=5)", array(50, $course_id));
+
+        // push student ids and seat nums to array
+        $stdids = array();
+        $seatnos = array();
+        foreach($recStudents as $records){
+            $id = $records->sid;
+            $seatno = $records->seatnum ;
+            array_push($stdids,$id);
+            array_push($seatnos,$seatno);
+        }
+        $add=1;
+        $arr=array();
+        $arr=["Seatno","Marks"];
+
+        //Creating file type
+        $inputFileName = "Assignment_Grading_sheet.xlsx";
+        
+        // for XLSX files
+        $writer = WriterFactory::create(Type::XLSX);
+        //$writer = WriterFactory::create(Type::CSV); // for CSV files
+        //$writer = WriterFactory::create(Type::ODS); // for ODS files
+        $tempfile=$writer->openToFile($inputFileName); // write data to a file or to a PHP stream
+        //$writer->openToBrowser($fileName); // stream data directly to the browser
+        $writer->addRow($arr);
+        foreach($seatnos as $seat)
+            $writer->addRow([$seat]); // add a row at a time
+        //$writer->addRows($multipleRows); // add multiple rows at a time
+        //$writer->setTempFolder('E:/');
+        $writer->close();
+        header('Content-Type:Assignment_Grading_sheet/xlsx');
+        header('Content-disposition: attachment; filename="'.$inputFileName.'";');
+        readfile("Assignment_Grading_sheet.xlsx"); 
+}
+?>
---
+++ teacher/print_grading_sheet_assign_pro.php
@@ -0,0 +1,66 @@
+<script src="../script/jquery/jquery-3.2.1.js"></script>
+
+<?php
+    require_once('../../../config.php');
+    $context = context_system::instance();
+    $PAGE->set_context($context);
+    $PAGE->set_pagelayout('standard');
+    $PAGE->set_title("Empty Grading Sheet");
+    $PAGE->set_heading("Print Empty Grading Sheet");
+    $PAGE->set_url($CFG->wwwroot.'/local/ned_obe/teacher/print_grading_sheet_assign_pro.php');
+    
+    require_login();
+    if($SESSION->oberole != "teacher"){
+        header('Location: ../index.php');
+    }
+    echo $OUTPUT->header();
+    ?>
+    <link rel="stylesheet" type="text/css" href="../css/cool-link/style.css" />
+    <?php
+    if(!empty($_GET['type']) && !empty($_GET['course']))
+    {
+        $course_id=$_GET['course'];
+		//echo "Course ID : $course_id";
+		$course_id = (int)$course_id; // convert course id from string to int
+		//echo gettype($course_id), "\n";
+        $type=$_GET['type'];
+        //echo " Activity Type : $type";
+
+        if($type=="assign"){
+            echo "<h3>Choose Assignment</h3><br>";
+            $mod=-4;
+        }
+        elseif($type=="project"){
+            echo "<h3>Choose Project</h3><br>";
+            $mod=-5;
+        }
+        
+        $rec=$DB->get_records_sql('SELECT * FROM mdl_manual_assign_pro WHERE courseid = ? AND module = ?', array($course_id,$mod));
+        
+        if($rec){
+            $serialno = 0;
+            $table = new html_table();
+            $table->head = array('S. No.', 'Activity Name');
+            foreach ($rec as $records) {
+                $serialno++;
+                $id = $records->id;
+                $name = $records->name;
+                
+                $table->data[] = array($serialno,"<a href='./export2.php?id=$id&course=$course_id'>$name</a>");
+            }
+            
+            echo html_writer::table($table);
+            echo "<br />";
+        }
+        else{
+            echo "<h3>You do not have any manual $type in this course!</h3>";
+        }
+    }
+	else
+	{?>
+		<h3 style="color:red;"> Invalid Selection </h3>
+    	<a href="../index.php">Back</a>
+    	<?php
+    }
+    echo $OUTPUT->footer();
+?>
