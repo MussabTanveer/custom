9ae2eb092285df4abdb91cf3f760dc04f3102d39
MussabTanveer <mussab.tanveer9@gmail.com>
2018-03-17 23:32:03 +0500

    grading form for quiz, mid, final using method POST

---
+++ teacher/grading_form.php
@@ -0,0 +1,131 @@
+<?php 
+   require_once('../../../config.php');
+    $context = context_system::instance();
+    $PAGE->set_context($context);
+    $PAGE->set_pagelayout('standard');
+    $PAGE->set_title("Online Grading Form");
+    $PAGE->set_heading("Online Grading Form");
+    $PAGE->set_url($CFG->wwwroot.'/local/ned_obe/teacher/grading_form.php');
+    
+    require_login();
+    if($SESSION->oberole != "teacher"){
+        header('Location: ../index.php');
+    }
+    echo $OUTPUT->header();
+?>
+<style>
+	input[type='number'] {
+		-moz-appearance:textfield;
+        max-width: 50px;
+        border: none;
+	}
+    input[type='number']:focus {
+        outline: none;
+        border: none;
+	}
+	input::-webkit-outer-spin-button,
+	input::-webkit-inner-spin-button {
+		-webkit-appearance: none;
+	}
+</style>
+<?php
+if(!empty($_GET['quiz']) && !empty($_GET['courseid']))
+{
+    $course_id=$_GET['courseid'];
+    $coursecontext = context_course::instance($course_id);
+    is_enrolled($coursecontext, $USER->id) || die('<h3>You are not enrolled in this course!</h3>'.$OUTPUT->footer());
+
+    $quizId= $_GET['quiz'];
+    $stdids=array();
+    $qids = array();
+    $quesnames=array();
+    $maxmarks=array();
+    $ques=$DB->get_records_sql("SELECT * FROM mdl_manual_quiz_question  WHERE mquizid=$quizId");
+    $qcount = 0;
+    if($ques)
+    {
+        foreach ($ques as $q) {
+            $qname = $q->quesname;
+            $id = $q->id;
+            $max = $q->maxmark;
+            array_push ($quesnames,$qname);
+            array_push ($qids,$id);
+            array_push ($maxmarks,$max);
+            $qcount++;
+        }
+    }
+    
+    ?>
+    <form method="post" action="insert_result.php">
+        <table border='10' cellpadding='8' id ="mytable">
+        <tr>
+        <th> Roll No </th>
+        <?php
+        foreach ($quesnames as $qname){
+            ?><th> <?php echo $qname ; ?> </th>
+            <?php
+        }
+        ?>
+        </tr>
+        <?php
+    
+        $users=$DB->get_records_sql("SELECT u.id AS sid, u.username AS seatnum, u.firstname, u.lastname
+        FROM mdl_role_assignments ra, mdl_user u, mdl_course c, mdl_context cxt
+        WHERE ra.userid = u.id
+        AND ra.contextid = cxt.id
+        AND cxt.contextlevel = 50
+        AND cxt.instanceid = c.id
+        AND c.id = $course_id
+        AND (roleid=5)");
+        
+        if($users)
+        {
+            foreach ($users as $user ) {
+            ?>
+            <tr>
+                <td>
+                    <?php echo $user->seatnum; array_push ($stdids,$user->sid); ?>
+                </td>
+                <?php
+                $i=0;
+                foreach ($quesnames as $qname){
+                ?>
+                <td style="background-color: #ECEEEF;">
+                    <input type="number" name="marks[]" step="0.001" min="0" max="<?php echo $maxmarks[$i]; ?>" required />
+                </td >
+                <?php
+                $i++;
+                }
+                ?>
+            </tr>
+            <?php
+          }
+        }
+        ?>
+        </table>
+        <input type="hidden" value='<?php echo $qcount; ?>' name="quescount">
+        <input type="hidden" value='<?php echo $quizId; ?>' name="quizid">
+        <?php
+        foreach($qids as $qid)
+        {
+        echo '<input type="hidden" name="quesid[]" value="'. $qid. '">';
+        }
+        foreach($stdids as $sid)
+        {
+        echo '<input type="hidden" name="studid[]" value="'. $sid. '">';
+        }
+        ?>
+        <br />
+        <input type="submit" value="Submit Result" name="submit" class="btn btn-primary">
+    </form>
+    
+    <?php
+    }
+    else
+    {?>
+        <h2 style="color:red;"> Invalid Selection </h2>
+        <a href="./teacher_courses.php">Back</a>
+    <?php
+    }
+    echo $OUTPUT->footer();
+    ?>
---
+++ teacher/insert_result.php
@@ -0,0 +1,82 @@
+<?php
+    require_once('../../../config.php');
+    $context = context_system::instance();
+    $PAGE->set_context($context);
+    $PAGE->set_pagelayout('standard');
+    $PAGE->set_title("Upload Result");
+    $PAGE->set_heading("Upload Result");
+    $PAGE->set_url($CFG->wwwroot.'/local/ned_obe/teacher/insert_result.php');
+    
+    require_login();
+    if($SESSION->oberole != "teacher"){
+        header('Location: ../index.php');
+    }
+    echo $OUTPUT->header();
+
+    if(isset($_POST['submit']))
+	{
+        // GET DATA
+		$ques_count = $_POST['quescount'];
+		$quiz_id = $_POST['quizid'];
+        $qidarray = array();
+		foreach ($_POST['quesid'] as $qid)
+		{
+			array_push($qidarray,$qid);
+        }
+        $sidarray = array();
+		foreach ($_POST['studid'] as $sid)
+		{
+			array_push($sidarray,$sid);
+        }
+        $marksarray = array();
+		foreach ($_POST['marks'] as $mobt)
+		{
+			array_push($marksarray,$mobt);	
+        }
+        //var_dump ($ques_count); echo "<br>";
+        //var_dump ($quiz_id); echo "<br>";
+        //var_dump ($qidarray); echo "<br>";
+        //var_dump ($sidarray); echo "<br>";
+        //var_dump ($marksarray); echo "<br>";
+
+        // INSERT DATA
+        $i = 0; // initialize
+        $qidx = 0; // to track ques index
+        $qcount=0; // to track ques count
+        try {
+            $transaction = $DB->start_delegated_transaction();
+            for ($j=0 ; $j<sizeof($sidarray); $j++){ // loop stud id times
+                for (; $i<sizeof($marksarray) ; $i++){ // loop marks obt time (for inserting marks of particular stud ques count times)
+                    $qcount++;
+                    $record = new stdClass();
+                    $record->quizid = $quiz_id;
+                    $record->userid = $sidarray[$j];
+                    $record->questionid = $qidarray[$qidx];
+                    $record->obtmark = $marksarray[$i];
+                    $DB->insert_record('manual_quiz_attempt', $record);
+                    //$sql="INSERT INTO mdl_manual_quiz_attempt (quizid,userid,questionid,obtmark) VALUES ('$quiz_id','$sidarray[$j]','$qidarray[$qidx]','$marksarray[$i]')";
+                    //$DB->execute($sql);
+                    $qidx++; // next ques id index
+                    if($qcount == $ques_count){
+                        $i++;
+                        $qidx =0; // first ques id index
+                        $qcount=0; // set ques count to 0
+                        break;
+                    }
+                }
+            }
+            $transaction->allow_commit();
+            echo "<font color = green > Result has been uploaded! </font> <br>";
+        } catch(Exception $e) {
+            $transaction->rollback($e);
+            echo "<font color = red > Failed to upload result! </font> <br>";
+        }
+    }
+    else{
+        echo "<font color = red > Invalid form submission! </font> <br>";
+    }
+    ?>
+    <a href="./teacher_courses.php">Back</a>
+    <?php
+        echo $OUTPUT->footer();
+    ?>
