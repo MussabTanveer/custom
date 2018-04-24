956eabd0079a58553f68e72af8ea95ef5ea7b75f
MussabTanveer <mussab.tanveer9@gmail.com>
2018-04-25 00:00:13 +0500

    update itm links

--- itm/report_itm.php
+++ itm/report_itm.php
@@ -21,22 +21,19 @@
 
         <a href="./select_frameworktoCourse.php" class="cool-link">Create Courses &amp; Map CLOs</a><br><br>
 
-
         <a href="../../../course/management.php" class="cool-link">Delete/Edit Courses</a><br><br>
         <!--<a href="./select_frameworktopracCourse.php" class="cool-link">Create Practical Courses &amp; Map CLOs</a><br><br>-->
 
         <!--<a href="./select_course.php" class="cool-link">Add CLOs to Courses</a><br><br>-->
 
-        <a href="../../../user/editadvanced.php?id=-1" class="cool-link">Add a new User</a><br><br>
+        <a href="../../../cohort/index.php" class="cool-link">Add/Edit/Delete/View Cohorts</a><br><br>
 
-       
-
+        <a href="../../../user/editadvanced.php?id=-1" class="cool-link">Add a New User</a><br><br>
+        
         <a href="../../../admin/tool/uploaduser/index.php" class="cool-link">Upload Users</a><br><br>
 
         <a href="../../../admin/user.php" class="cool-link">View/Edit/Delete Users</a><br><br>
-
-        <a href="../../../cohort/index.php" class="cool-link">Add/Edit/View Cohorts</a><br><br>
-
+        
         <a href="./select_course_enrol.php" class="cool-link">Enrol/Unenrol Users from Courses</a><br><br>
     </div>
 <?php
