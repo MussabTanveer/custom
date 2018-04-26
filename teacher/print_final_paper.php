f79a28400d6503f03e3ea2e2f9429327b3ef5cbb
MussabTanveer <mussab.tanveer9@gmail.com>
2018-04-27 00:47:13 +0500

    use font awesome icons instead edit/del imgs

--- teacher/print_final_paper.php
+++ teacher/print_final_paper.php
@@ -28,18 +28,16 @@
         {
             $serialno = 0;
             $table = new html_table();
-            $table->head = array('S. No.', 'Final Exam Name','Print Final Exam Paper','Delete','Edit');
+            $table->head = array('S. No.', 'Print Final Exam Details','Print Final Exam Paper','Delete','Edit');
             foreach ($finals as $records) {
                 $serialno++;
                 $qid = $records->id;
                 $qname = $records->name;
                  $mime = $records->mime;
                 if ($mime)
-                
-                    $table->data[] = array($serialno,"<a href='./print_final.php?quiz=$qid&courseid=$course_id'>Print $qname</a>","<a href='./print_uploaded_paper.php?quiz=$qid&courseid=$course_id'>Print $qname</a>","<a href='./delete_manual_activity.php?id=$qid&course=$course_id&type=$type' title='Delete' onClick=\"return confirm('Are you sure you want to delete this activity and its attempt?')\" > <img src='../img/icons/Delete1.png' /></a>","<a href='./edit_manual_activity.php?id=$qid&course=$course_id&type=$type' title='Edit') > <img src='../img/icons/Edit.png' /></a>");
+                    $table->data[] = array($serialno,"<a href='./print_final.php?quiz=$qid&courseid=$course_id'>Print $qname</a>","<a href='./print_uploaded_paper.php?quiz=$qid&courseid=$course_id'>Print $qname</a>","<a href='./delete_manual_activity.php?id=$qid&course=$course_id&type=$type' title='Delete' onClick=\"return confirm('Are you sure you want to delete this activity and its attempt?')\" ><i class='icon fa fa-trash text-danger' aria-hidden='true' title='Delete' aria-label='Delete'></i></a>","<a href='./edit_manual_activity.php?id=$qid&course=$course_id&type=$type' title='Edit') ><i class='icon fa fa-pencil text-info' aria-hidden='true' title='Edit' aria-label='Edit'></i></a>");
                 else
-                    $table->data[] = array($serialno,"<a href='./print_final.php?quiz=$qid&courseid=$course_id'>Print $qname</a>",'-',"<a href='./delete_manual_activity.php?id=$qid&course=$course_id&type=$type' title='Delete' onClick=\"return confirm('Are you sure you want to delete this activity and its attempt?')\" > <img src='../img/icons/Delete1.png' /></a>","<a href='./edit_manual_activity.php?id=$qid&course=$course_id&type=$type' title='Edit') > <img src='../img/icons/Edit.png' /></a>");
-            
+                    $table->data[] = array($serialno,"<a href='./print_final.php?quiz=$qid&courseid=$course_id'>Print $qname</a>",'-',"<a href='./delete_manual_activity.php?id=$qid&course=$course_id&type=$type' title='Delete' onClick=\"return confirm('Are you sure you want to delete this activity and its attempt?')\" ><i class='icon fa fa-trash text-danger' aria-hidden='true' title='Delete' aria-label='Delete'></i></a>","<a href='./edit_manual_activity.php?id=$qid&course=$course_id&type=$type' title='Edit') ><i class='icon fa fa-pencil text-info' aria-hidden='true' title='Edit' aria-label='Edit'></i></a>");
             }
 
             echo html_writer::table($table);
--- teacher/print_mid_paper.php
+++ teacher/print_mid_paper.php
@@ -1,6 +1,3 @@
-
-
-
 <?php 
    require_once('../../../config.php');
     $context = context_system::instance();
@@ -31,7 +28,7 @@
         {
             $serialno = 0;
             $table = new html_table();
-            $table->head = array('S. No.', 'Midterm Name','Print Mid Paper','Delete','Edit');
+            $table->head = array('S. No.', 'Print Midterm Details','Print Midterm Paper','Delete','Edit');
             foreach ($midterms as $records) {
                 $serialno++;
                 $qid = $records->id;
@@ -39,9 +36,9 @@
                 $mime = $records->mime;
                 if ($mime)
                 
-                    $table->data[] = array($serialno,"<a href='./print_mid.php?quiz=$qid&courseid=$course_id'>Print $qname</a>","<a href='./print_uploaded_paper.php?quiz=$qid&courseid=$course_id'>Print $qname</a>","<a href='./delete_manual_activity.php?id=$qid&course=$course_id&type=$type' title='Delete' onClick=\"return confirm('Are you sure you want to delete this activity and its attempt?')\" > <img src='../img/icons/Delete1.png' /></a>","<a href='./edit_manual_activity.php?id=$qid&course=$course_id&type=$type' title='Edit') > <img src='../img/icons/Edit.png' /></a>");
+                    $table->data[] = array($serialno,"<a href='./print_mid.php?quiz=$qid&courseid=$course_id'>Print $qname</a>","<a href='./print_uploaded_paper.php?quiz=$qid&courseid=$course_id'>Print $qname</a>","<a href='./delete_manual_activity.php?id=$qid&course=$course_id&type=$type' title='Delete' onClick=\"return confirm('Are you sure you want to delete this activity and its attempt?')\" ><i class='icon fa fa-trash text-danger' aria-hidden='true' title='Delete' aria-label='Delete'></i></a>","<a href='./edit_manual_activity.php?id=$qid&course=$course_id&type=$type' title='Edit') ><i class='icon fa fa-pencil text-info' aria-hidden='true' title='Edit' aria-label='Edit'></i></a>");
                 else
-                    $table->data[] = array($serialno,"<a href='./print_mid.php?quiz=$qid&courseid=$course_id'>Print $qname</a>",'-',"<a href='./delete_manual_activity.php?id=$qid&course=$course_id&type=$type' title='Delete' onClick=\"return confirm('Are you sure you want to delete this activity and its attempt?')\" > <img src='../img/icons/Delete1.png' /></a>","<a href='./edit_manual_activity.php?id=$qid&course=$course_id&type=$type' title='Edit') > <img src='../img/icons/Edit.png' /></a>");
+                    $table->data[] = array($serialno,"<a href='./print_mid.php?quiz=$qid&courseid=$course_id'>Print $qname</a>",'-',"<a href='./delete_manual_activity.php?id=$qid&course=$course_id&type=$type' title='Delete' onClick=\"return confirm('Are you sure you want to delete this activity and its attempt?')\" ><i class='icon fa fa-trash text-danger' aria-hidden='true' title='Delete' aria-label='Delete'></i></a>","<a href='./edit_manual_activity.php?id=$qid&course=$course_id&type=$type' title='Edit') ><i class='icon fa fa-pencil text-info' aria-hidden='true' title='Edit' aria-label='Edit'></i></a>");
             
             }
 
