9d75f10a8668e049195bde77c1652ec074ff3b4f
MussabTanveer <mussab.tanveer9@gmail.com>
2018-05-19 20:56:28 +0500

    remove plo status

--- teacher/clo_wise_report.php
+++ teacher/clo_wise_report.php
@@ -24,7 +24,6 @@ th{
 }
 </style>
 <?php
-
     if(!empty($_GET['course']))
     {
         $course_id=$_GET['course'];
@@ -40,7 +39,6 @@ th{
         AND cxt.instanceid = c.id
         AND c.id = ?
         AND (roleid=5)", array(50, $course_id));
-
         $stdids = array();
         $seatnos = array();
         foreach($recStudents as $records){
@@ -49,95 +47,45 @@ th{
             array_push($stdids,$id);
             array_push($seatnos,$seatno);
         }
-
         //Get course clo with its plo, level, passing percentage
 		$courseclos=$DB->get_records_sql(
         "SELECT
-            clo.id AS cloid,
-            clo.shortname AS cloname,
-            plo.id AS ploid,
-            plo.idnumber AS ploidn,
-            clokpi.kpi AS clostudpass,
-            clocohortkpi.kpi AS clocohortpass,
-            plostudkpi.kpi AS plostudpass,
-            plocohortkpi.kpi AS plocohortpass,
-            taxlvl.level
+        clo.id AS cloid,
+        clo.shortname AS cloname,
+        plo.idnumber AS ploidn,
+        clokpi.kpi AS passpercent,
+        clocohortkpi.kpi AS cohortpasspercent,
+        taxlvl.level
         FROM
-            mdl_competency_coursecomp cc,
-            mdl_competency clo,
-            mdl_competency plo,
-            mdl_clo_kpi clokpi,
-            mdl_clo_cohort_kpi clocohortkpi,
-            mdl_plo_kpi_individual_student plostudkpi,
-            mdl_plo_kpi_cohort_course plocohortkpi,
-            mdl_taxonomy_clo_level taxclolvl,
-            mdl_taxonomy_levels taxlvl
+        mdl_competency_coursecomp cc,
+        mdl_competency clo,
+        mdl_competency plo,
+        mdl_clo_kpi clokpi,
+        mdl_clo_cohort_kpi clocohortkpi,
+        mdl_taxonomy_clo_level taxclolvl,
+        mdl_taxonomy_levels taxlvl
         WHERE
-            cc.courseid = ? AND
-            cc.competencyid=clo.id AND
-            clo.id=clokpi.cloid AND
-            clo.id=clocohortkpi.cloid AND
-            plo.id=plostudkpi.ploid AND
-            plo.id=plocohortkpi.ploid AND
-            plo.id=clo.parentid AND
-            clo.id=taxclolvl.cloid AND
-            taxclolvl.levelid=taxlvl.id",
+        cc.courseid = ? AND cc.competencyid=clo.id AND clo.id=clokpi.cloid AND clo.id=clocohortkpi.cloid AND plo.id=clo.parentid AND clo.id=taxclolvl.cloid AND taxclolvl.levelid=taxlvl.id",
         array($course_id));
             
-        $clonames = array(); $plonames = array(); $lnames = array(); $closid = array(); $plosid = array(); $clospasspercent = array(); $clocohortpasspercent = array(); $plospasspercent = array(); $plocohortpasspercent = array();
+        $clonames = array(); $plonames = array(); $lnames = array(); $closid = array(); $clospasspercent = array(); $clocohortpasspercent = array();
         foreach ($courseclos as $recC) {
             $cid = $recC->cloid;
             $clo = $recC->cloname;
-            $pid = $recC->ploid;
             $plo = $recC->ploidn;
             $level = $recC->level;
-            $closp = $recC->clostudpass;
-            $clocp = $recC->clocohortpass;
-            $plosp = $recC->plostudpass;
-            $plocp = $recC->plocohortpass;
+            $pp = $recC->passpercent;
+            $cp = $recC->cohortpasspercent;
             array_push($closid, $cid); // array of clo ids
             array_push($clonames, $clo); // array of clo names
-            array_push($plosid, $pid); // array of plo ids
             array_push($plonames, $plo); // array of plo idnum
             array_push($lnames, $level); // array of levels
-            array_push($clospasspercent, $closp); // array of clo individual stud pass percent
-            array_push($clocohortpasspercent, $clocp); // array of clo cohort course pass percent
-            array_push($plospasspercent, $plosp); // array of plo individual stud pass percent
-            array_push($plocohortpasspercent, $plocp); // array of plo cohort course pass percent
+            array_push($clospasspercent, $pp); // array of clo individual stud pass percent
+            array_push($clocohortpasspercent, $cp); // array of clo cohort course pass percent
         }
-        $uniqueploids = array_unique($plosid);
-        $plosclo = array();
-
-        for($i=0; $i<count($uniqueploids); $i++){
-            $k=0;
-            for($j=0; $j<count($closid); $j++){
-                if($plosid[$j] == $uniqueploids[$i]){
-                    $plosclo[$i][$k] = $closid[$i];
-                    $k++;
-                }
-            }
-        }
-        /*var_dump($uniqueploids); echo "<br>";
-        var_dump($plosclo);*/
-
-        $uniqueplospasspercent = array();
-        for ($i=0; $i < count($uniqueploids); $i++) { 
-            $key = array_search($uniqueploids[$i], $plosid);
-            array_push($uniqueplospasspercent, $plospasspercent[$key]);
-        }
-        /*echo "<br>";
-        var_dump($uniqueplospasspercent);*/
-        $uniqueplocohortpasspercent = array();
-        for ($i=0; $i < count($uniqueploids); $i++) { 
-            $key = array_search($uniqueploids[$i], $plosid);
-            array_push($uniqueplocohortpasspercent, $plocohortpasspercent[$key]);
-        }
-
-
         $closidCountActivity = array();
         for($j=0; $j<count($closid); $j++)
             $closidCountActivity[$j]=0;
-
         // Get course online quiz ids
         $courseQuizId=$DB->get_records_sql("SELECT * FROM `mdl_quiz` WHERE course = ? ", array($course_id));
         $quizids = array();
@@ -153,7 +101,6 @@ th{
             $id = $aid->id;
             array_push($assignids, $id); // array of assign ids
         }
-
         // Get attempted course manual quiz/midterm/final ids
         $courseMQuizId=$DB->get_records_sql("SELECT * FROM `mdl_manual_quiz` WHERE courseid = ? AND id IN (SELECT quizid FROM `mdl_manual_quiz_attempt`)", array($course_id));
         $mquizids = array();
@@ -162,7 +109,6 @@ th{
             array_push($mquizids, $id); // array of quiz/mt/final ids
         }
         //print_r($mquizids);
-
         // Get attempted course manual assignment/project ids
         $courseMAssignId=$DB->get_records_sql("SELECT * FROM `mdl_manual_quiz` WHERE courseid = ? AND id IN (SELECT assignproid FROM `mdl_manual_assign_pro_attempt`)", array($course_id));
         $massignids = array();
@@ -209,7 +155,6 @@ th{
             ORDER BY qa.userid, qu.competencyid',
             
             array($quizids[$i],1));
-
             $seatnosQ = array();
             $closQ = array();
             $resultQ = array();
@@ -233,14 +178,12 @@ th{
             }
             array_push($quiznames,$quizname);
             $cloQuizUnique = array_unique($closQ);
-
             array_push($cloQCount,count($cloQuizUnique));
             array_push($seatnosQMulti,$seatnosQ);
             array_push($closUniqueQMulti,$cloQuizUnique);
             array_push($closQMulti,$closQ);
             array_push($resultQMulti,$resultQ);
         }
-
         // MANUAL QUIZ/MIDTERM/FINAL
         for($i=0; $i < count($mquizids); $i++){
             $recMQuiz=$DB->get_recordset_sql(
@@ -263,7 +206,6 @@ th{
             ORDER BY qa.userid, qu.cloid',
             
             array($mquizids[$i]));
-
             $seatnosQ = array();
             $closQ = array();
             $resultQ = array();
@@ -287,7 +229,6 @@ th{
             }
             array_push($quiznames,$quizname);
             $cloQuizUnique = array_unique($closQ);
-
             array_push($cloQCount,count($cloQuizUnique));
             array_push($seatnosQMulti,$seatnosQ);
             array_push($closUniqueQMulti,$cloQuizUnique);
@@ -298,7 +239,6 @@ th{
         echo "<br><br>"; print_r($quiznames); echo "<br><br>"; print_r($cloQCount);
         echo "<br><br>"; print_r($seatnosQMulti); echo "<br><br>"; print_r($closUniqueQMulti);
         echo "<br><br>"; print_r($closQMulti); echo "<br><br>"; print_r($resultQMulti);*/
-
         /**** ONLINE+MANUAL ASSIGNMENTS ****/
         // Find students assignment records
         $seatnosAMulti = array();
@@ -329,7 +269,6 @@ th{
                 ORDER BY ag.userid',
                 
             array($assignids[$i],-1,$course_id,1));
-
             $seatnosA = array();
             $closA = array();
             $resultA = array();
@@ -351,18 +290,14 @@ th{
                 array_push($seatnosA,$un);
                 array_push($closA,$clo);
             }
-
             array_push($assignnames,$assignname);
             $cloAssignUnique = array_unique($closA);
-
             array_push($seatnosAMulti,$seatnosA);
             array_push($closAMulti,$closA);
             array_push($resultAMulti,$resultA);
-
             array_push($cloACount,count($cloAssignUnique));
             array_push($closUniqueAMulti,$cloAssignUnique);
         }
-
         // MANUAL ASSIGNMENTS/PROJECTS
         for($i=0; $i < count($massignids); $i++){
             // Get assign records
@@ -382,7 +317,6 @@ th{
                 ORDER BY att.userid',
                 
             array($massignids[$i]));
-
             $seatnosA = array();
             $closA = array();
             $resultA = array();
@@ -404,14 +338,11 @@ th{
                 array_push($seatnosA,$un);
                 array_push($closA,$clo);
             }
-
             array_push($assignnames,$assignname);
             $cloAssignUnique = array_unique($closA);
-
             array_push($seatnosAMulti,$seatnosA);
             array_push($closAMulti,$closA);
             array_push($resultAMulti,$resultA);
-
             array_push($cloACount,count($cloAssignUnique));
             array_push($closUniqueAMulti,$cloAssignUnique);
         }
@@ -439,11 +370,8 @@ th{
                 <?php
                 }
             }
-            $uniqueplonames = array_unique($plonames);
-            $uniqueplonames =  array_values($uniqueplonames);//Reindexing the array.
-           // var_dump($uniqueplonames);            ?>
+            ?>
             <th colspan="<?php echo count($closid); ?>">CLO Status (pass/fail)</th>
-            <th colspan="<?php echo count($uniqueplonames); ?>">PLO Status (pass/fail)</th>
         </tr>
         <tr>
             <th></th>
@@ -470,22 +398,12 @@ th{
                 <th><?php echo $clonames[$i]; ?></th>
                 <?php
             }
-            /****** PLOS ******/
-            for($i=0; $i<count($uniqueplonames); $i++) {
-                ?>
-                <th><?php echo $uniqueplonames[$i]; //var_dump($uniqueplonames) ?></th>
-                <?php
-            }
             ?>
         </tr>
         <?php
         $cohort_clo_stat = array(); // cohort course clo status -> increment for pass
         for($i=0; $i<count($closid); $i++)
             $cohort_clo_stat[$i] = 0; // initialize all clos status with 0
-
-        $cohort_plo_stat = array(); // cohort course plo status -> increment for pass
-        for($i=0; $i<count($uniqueploids); $i++)
-            $cohort_plo_stat[$i] = 0; // initialize all plos status with 0
         
         foreach ($seatnos as $seatno) {
         $ind_stud_clo_stat = array(); // individual student clo status -> 1 for pass, 0 for fail
@@ -507,11 +425,11 @@ th{
                                 $flag=1;
                                 //if($resultQMulti[$j][$k] == 'P')
                                 if($resultQMulti[$j][$k] >= $clospasspercent[$i]){
-                                    echo "<td><i class='fa fa-square' aria-hidden='true' style='color: #05E177'></i><span style='display: none'>P</span></td>";
+                                    echo "<td><i class='fa fa-square' aria-hidden='true' style='color: #05E177'><span style='display: none'>P</span></i></td>";
                                     $ind_stud_clo_stat[$i] = 1; // set status pass
                                 }
                                 else
-                                    echo "<td><i class='fa fa-square' aria-hidden='true' style='color: #FE3939'></i><span style='display: none'>F</span></td>";
+                                    echo "<td><i class='fa fa-square' aria-hidden='true' style='color: #FE3939'><span style='display: none'>F</span></i></td>";
                             }
                         }
                         if($flag==0)
@@ -528,11 +446,11 @@ th{
                                 $flag=1;
                                 //if($resultAMulti[$j][$k] == 'P')
                                 if($resultAMulti[$j][$k] >= $clospasspercent[$i]){
-                                    echo "<td><i class='fa fa-square' aria-hidden='true' style='color: #05E177'></i><span style='display: none'>P</span></td>";
+                                    echo "<td><i class='fa fa-square' aria-hidden='true' style='color: #05E177'><span style='display: none'>P</span></i></td>";
                                     $ind_stud_clo_stat[$i] = 1; // set status pass
                                 }
                                 else
-                                    echo "<td><i class='fa fa-square' aria-hidden='true' style='color: #FE3939'></i><span style='display: none'>F</span></td>";
+                                    echo "<td><i class='fa fa-square' aria-hidden='true' style='color: #FE3939'><span style='display: none'>F</span></i></td>";
                             }
                         }
                         if($flag==0)
@@ -542,66 +460,18 @@ th{
                     }
             }
             /****** Student CLOS status ******/
-            $ind_stud_plo_stat = array(); // individual student plo status -> 1 for pass, 0 for fail
-            $uniqueploids = array_values($uniqueploids); //Reindexing the array.
-            
-            for($i=0; $i<count($uniqueploids); $i++){
-                $k=0;
-                for($j=0; $j<count($closid); $j++){
-                    if($plosid[$j] == $uniqueploids[$i]){
-                       // var_dump($uniqueploids);
-                        $ind_stud_plo_stat[$i][$k] = 0; // set all plos status to fail
-                        $k++;
-                    }
-                }
-            }
-            /*echo "<br>FAIL  ";
-            var_dump($ind_stud_plo_stat);*/
-            
             for($i=0; $i<count($closid); $i++) {
                 if($ind_stud_clo_stat[$i]){
-                    // for stud plo status logic
-                    $a=0;
-                    foreach($plosclo as $pc){
-                        for($b=0; $b<count($pc); $b++){
-                            if($pc[$b] == $closid[$i]){
-                                $ind_stud_plo_stat[$a][$b] = 1; // set plo status to pass
-                            }
-                        }
-                        $a++;
-                    }
-                    // stud plo status logic ends
-                    echo "<td><i class='fa fa-square' aria-hidden='true' style='color: #05E177'></i><span style='display: none'>P</span></td>";
+                    echo "<td><i class='fa fa-square' aria-hidden='true' style='color: #05E177'><span style='display: none'>P</span></i></td>";
                     $cohort_clo_stat[$i]++;
                 }
                 else
                     echo "<td><i class='fa fa-square' aria-hidden='true' style='color: #FE3939'><span style='display: none'>F</span></i></td>";
             }
-            /*echo "<br>PASS ";
-            var_dump($ind_stud_plo_stat);*/
-
-            /****** Student PLOS status ******/
-            $a=0; $countpassclo = 0;
-            foreach($ind_stud_plo_stat as $splo){
-                for($b=0; $b<count($splo); $b++){
-                    if($ind_stud_plo_stat[$a][$b] == 1){
-                        $countpassclo++;
-                    }
-                }
-                //echo $countpassclo;
-                if(($countpassclo/count($splo))*100 >= $uniqueplospasspercent[$a]){
-                    echo "<td><i class='fa fa-square' aria-hidden='true' style='color: #05E177'></i><span style='display: none'>P</span></td>";
-                    $cohort_plo_stat[$a]++;
-                }
-                else
-                    echo "<td><i class='fa fa-square' aria-hidden='true' style='color: #FE3939'></i><span style='display: none'>F</span></td>";
-                $a++; $countpassclo = 0;
-            }
             ?>
         </tr>
         <?php
         }
-
         // Total Colspan for last 2 rows
         $colspan = 0;
         for($i=0; $i<count($closid); $i++) {
@@ -617,10 +487,6 @@ th{
             for($i=0; $i<count($closid); $i++) {
                 echo "<td>".(($cohort_clo_stat[$i]/count($recStudents))*100)."%</td>";
             }
-            /****** Course PLOS status (Quantitative) ******/
-            for($i=0; $i<count($uniqueploids); $i++) {
-                echo "<td>".(($cohort_plo_stat[$i]/count($recStudents))*100)."%</td>";
-            }
             ?>
         </tr>
         <tr>
@@ -630,16 +496,9 @@ th{
             /****** Course CLOS status (pass/fail) ******/
             for($i=0; $i<count($closid); $i++) {
                 if(($cohort_clo_stat[$i]/count($recStudents))*100 >= $clocohortpasspercent[$i])
-                    echo "<td><i class='fa fa-square' aria-hidden='true' style='color: #05E177'></i><span style='display: none'>P</span></td>";
+                    echo "<td><i class='fa fa-square' aria-hidden='true' style='color: #05E177'><span style='display: none'>P</span></i></td>";
                 else
-                    echo "<td><i class='fa fa-square' aria-hidden='true' style='color: #FE3939'></i><span style='display: none'>F</span></td>";
-            }
-            /****** Course PLOS status (pass/fail) ******/
-            for($i=0; $i<count($uniqueploids); $i++) {
-                if(($cohort_plo_stat[$i]/count($recStudents))*100 >= $uniqueplocohortpasspercent[$i])
-                    echo "<td><i class='fa fa-square' aria-hidden='true' style='color: #05E177'></i><span style='display: none'>P</span></td>";
-                else
-                    echo "<td><i class='fa fa-square' aria-hidden='true' style='color: #FE3939'></i><span style='display: none'>F</span></td>";
+                    echo "<td><i class='fa fa-square' aria-hidden='true' style='color: #FE3939'><span style='display: none'>F</span></i></td>";
             }
             ?>
         </tr>
