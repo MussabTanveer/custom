ca6ba972b3750c0157027aa65d6026319ca9332e
MussabTanveer <mussab.tanveer9@gmail.com>
2018-05-23 22:25:32 +0500

    correction in header text

--- teacher/view_other2.php
+++ teacher/view_other2.php
@@ -5,109 +5,83 @@
     $context = context_system::instance();
     $PAGE->set_context($context);
     $PAGE->set_pagelayout('standard');
-    $PAGE->set_title("Assignment Results");
-    $PAGE->set_heading("Assignment Results");
+    $PAGE->set_title("Other Activity Results");
+    $PAGE->set_heading("Other Activity Results");
     $PAGE->set_url($CFG->wwwroot.'/local/ned_obe/teacher/view_other2.php');
 
-       header('Content-Type: text/plain');
-   
+    header('Content-Type: text/plain');
+    
 
-      require_login();
+    require_login();
     if($SESSION->oberole != "teacher"){
         header('Location: ../index.php');
     }
     echo $OUTPUT->header();
 
-if(isset($_GET['otherid']))
+    if(!empty($_GET['otherid']))
     {
-$other_id=$_GET['otherid'];
-//echo $other_id;
+    $other_id=$_GET['otherid'];
+    //echo $other_id;
+    
+    //$id=$_POST['id'];
+    //echo $id;
+    $rec1=$DB->get_recordset_sql('SELECT ma.name,ma.maxmark,comp.idnumber,ma.cloid, comp.id from mdl_manual_other ma,mdl_competency comp WHERE comp.id=ma.cloid AND ma.id=?',array($other_id));
 
-
-//$id=$_POST['id'];
-//echo $id;
-$rec1=$DB->get_recordset_sql('SELECT ma.name,ma.maxmark,comp.idnumber,ma.cloid, comp.id from mdl_manual_other ma,mdl_competency comp WHERE comp.id=ma.cloid AND ma.id=?',array($other_id));
-
-if($rec1){
-
- foreach ($rec1 as $records) {
-                
+    if($rec1){
+        foreach ($rec1 as $records) {
                 $name = $records->name;
                 $clo=$records->idnumber;
                 $maxmark=$records->maxmark;
+        }
+        echo "<h3>".$name." "."(".$clo.")"."</h3>";
+        echo "<h3>"."Max Marks:"." ".$maxmark."</h3>";
+    }
 
-
-}
-
-
-echo "<h3>".$name." "."(".$clo.")"."</h3>";
-
-
-echo "<h3>"."Max Marks:"." ".$maxmark."</h3>";
-}
-
-else{
-
-
-	echo "No record present!";
-}
-$rec=$DB->get_recordset_sql(
+    else{
+	    echo "No record present!";
+    }
+    $rec=$DB->get_recordset_sql(
         'SELECT us.username,us.id,maa.obtmark, ma.id,maa.id from mdl_manual_other_attempt maa , mdl_manual_other ma, mdl_user us where us.id=maa.userid AND ma.id=maa.otherid  AND ma.id= ? AND ma.module=? ',array($other_id,'-6'));
 
-if($rec){
-
- 
-              $serialno = 0;
-            $table = new html_table();
-            $table->id = "mytable";
-     $table->head = array('S. No.', 'Seat No.', 'Marks Obtained','Edit');
-
-
-
-     foreach ($rec as $records) {
-                $serialno++;
-                $marksid=$records->id;
-                $userid = $records->username;
-                $obtmark = $records->obtmark;
-                //"<a href='edit_assignment_marks.php?edit=$marksid' title='Edit'><img src='../img/icons/edit.png' /></a>";
-$table->data[] = array($serialno,$userid,$obtmark, "<a href='edit_other_marks.php?edit=$marksid&userid=$userid&otherid=$other_id' title='Edit'><img src='../img/icons/edit.png' /></a>");
-
-            }
-
-
-echo html_writer::table($table);
-?>
-<button id="myButton" class="btn btn-success">Export to Excel</button>
-<!-- Export html Table to xls -->
-<script type="text/javascript" >
-    $(document).ready(function(e){
-        $("#myButton").click(function(e){ 
-            $("#mytable").table2excel({
-                name: "file name",
-                filename: "assignment_result",
-                fileext: ".xls"
-            });
-        });
-    });
-</script>
-<?php
-}
-
-  else{
-            echo "<h3>No students have attempted Assignment yet!</h3>";
+    if($rec){
+        $serialno = 0;
+        $table = new html_table();
+        $table->id = "mytable";
+        $table->head = array('S. No.', 'Seat No.', 'Marks Obtained','Edit');
+        foreach ($rec as $records) {
+            $serialno++;
+            $marksid=$records->id;
+            $userid = $records->username;
+            $obtmark = $records->obtmark;
+            //"<a href='edit_assignment_marks.php?edit=$marksid' title='Edit'><img src='../img/icons/edit.png' /></a>";
+            $table->data[] = array($serialno,$userid,$obtmark, "<a href='edit_other_marks.php?edit=$marksid&userid=$userid&otherid=$other_id' title='Edit'><img src='../img/icons/edit.png' /></a>");
         }
 
+    echo html_writer::table($table);
+    ?>
+    <button id="myButton" class="btn btn-success">Export to Excel</button>
+    <!-- Export html Table to xls -->
+    <script type="text/javascript" >
+        $(document).ready(function(e){
+            $("#myButton").click(function(e){ 
+                $("#mytable").table2excel({
+                    name: "file name",
+                    filename: "assignment_result",
+                    fileext: ".xls"
+                });
+            });
+        });
+    </script>
+    <?php
+    }
 
-
+    else{
+       echo "<h3>No students have attempted Assignment yet!</h3>";
+    }
 }
-
-
-
-
 
 ?>
 
-
- <?php 
-        echo $OUTPUT->footer();
-    ?>
+<?php 
+    echo $OUTPUT->footer();
+?>
