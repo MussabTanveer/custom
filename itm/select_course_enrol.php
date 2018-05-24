3e5547a7f4f17e32c0596fdde12eb18b7d5a4132
MussabTanveer <mussab.tanveer9@gmail.com>
2018-05-24 19:59:24 +0500

    style date input box and change its name

--- itm/select_course_enrol.php
+++ itm/select_course_enrol.php
@@ -21,41 +21,39 @@
     <form action="actionfile.php" class="form-inline">
         <h5 autofocus style="text-align:center;">Filter Records By</h5>
         <p style="text-align:center;">
-        <select autofocus id="Filtercheck" name="dropdown"  onChange="datepicker()" class="select custom-select">
-        <option value="coursecode">Course Code</option>
-        <option value="coursename">Course Name</option>
-        <option value="shortname">Short Name</option>
-        <option value="startdate">Start Date</option>
-        <option value="enddate">End Date</option>
+        <select autofocus id="Filtercheck" name="dropdown" onChange="datepicker()" class="select custom-select">
+            <option value="coursecode">Course Code</option>
+            <option value="coursename">Course Name</option>
+            <option value="shortname">Short Name</option>
+            <option value="startdate">Start Date</option>
+            <option value="enddate">End Date</option>
         </select>
-        <input type="date" name="id_enddate" id="id_enddate"  >
+        <input type="date" name="date" id="id_enddate" class="form-control">
         <input id="inputcheck" name="inputtext" type="text" placeholder="Enter Value" class="form-control">
         <input id="buttoncheck" type="submit" class="btn btn-info">
         <body onload="datepicker()">
-        
         </p>
     </form>
     <br />
     <script>
-
-var a=document.getElementById('Filtercheck').value;
-document.getElementById('id_enddate').style.display="none";
-    function datepicker(){
         var a=document.getElementById('Filtercheck').value;
-        if (a=='startdate' || a=='enddate'){
-            document.getElementById('inputcheck').style.display="none";
-            document.getElementById('id_enddate').style.display="inline";
+        document.getElementById('id_enddate').style.display="none";
+        function datepicker(){
+            var a=document.getElementById('Filtercheck').value;
+            if (a=='startdate' || a=='enddate'){
+                document.getElementById('inputcheck').style.display="none";
+                document.getElementById('id_enddate').style.display="inline";
+            }
+            else{
+                document.getElementById('inputcheck').style.display="inline";
+                document.getElementById('id_enddate').style.display="none";
+            }
         }
-        else{
-            document.getElementById('inputcheck').style.display="inline";
-            document.getElementById('id_enddate').style.display="none";
-        }
-    }
     </script>
 <?php
 
 	//Displaying all courses
-	$courses=$DB->get_records_sql('SELECT * FROM `mdl_course` WHERE id != ?', array(1));
+	$courses=$DB->get_records_sql('SELECT * FROM `mdl_course` WHERE id != ? ORDER BY id DESC', array(1));
     
     if($courses){
         $i = 1;
