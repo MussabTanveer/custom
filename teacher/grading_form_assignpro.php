0bf337bab2feb4622d2353aff7afe273f562ade9
MussabTanveer <mussab.tanveer9@gmail.com>
2018-05-27 14:41:03 +0500

    remove required attribute on marks input

--- teacher/grading_form_assignpro.php
+++ teacher/grading_form_assignpro.php
@@ -79,7 +79,7 @@ if(!empty($_GET['as_pro']) && !empty($_GET['courseid']))
                     <?php echo $user->seatnum; array_push ($stdids,$user->sid); ?>
                 </td>
                 <td style="background-color: #ECEEEF;">
-                    <input type="number" name="marks[<?php echo $indx; ?>]" step="0.001" min="0" max="<?php echo $maxmarks; ?>" required />
+                    <input type="number" name="marks[<?php echo $indx; ?>]" step="0.001" min="0" max="<?php echo $maxmarks; ?>" />
                 </td >
             </tr>
             <?php
