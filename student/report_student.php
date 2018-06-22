cce922a3765c4851a6af6c34d876ce56afbc30a6
MussabTanveer <mussab.tanveer9@gmail.com>
2018-06-23 02:03:40 +0500

    link to select semester

--- student/report_student.php
+++ student/report_student.php
@@ -20,7 +20,7 @@
 
         <a href="./display_courses_student.php" class="cool-link">My Activity CLO Report</a><br><br>
 
-        <a href="./display_semester_progress.php" class="cool-link">My Semester Progress Report</a></br><br>
+        <a href="./select_semester.php" class="cool-link">My Semester Progress Report</a></br><br>
 
         <a href="./display_course_progress.php" class="cool-link">My Overall Progress Report</a></br><br>
     </div>
