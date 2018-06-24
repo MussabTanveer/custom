564d47a23f8f494b0fdf387c419cab9ef39cb80f
MussabTanveer <mussab.tanveer9@gmail.com>
2018-06-25 01:52:15 +0500

    select course to edit

---
+++ itm/select_course_edit.php
@@ -0,0 +1,73 @@
+<?php
+    require_once('../../../config.php');
+    $context = context_system::instance();
+    $PAGE->set_context($context);
+    $PAGE->set_pagelayout('standard');
+    $PAGE->set_title("Choose Course");
+    $PAGE->set_heading("Choose Course to Edit");
+    $PAGE->set_url($CFG->wwwroot.'/local/ned_obe/itm/select_course_edit.php');
+    
+    require_login();
+    if($SESSION->oberole != "itm"){
+        header('Location: ../index.php');
+    }
+    echo $OUTPUT->header();
+
+    if(isset($_POST['inputtext'])){
+        $nam=$_POST['inputtext'];
+    }
+?>
+
+    <form action="actionfile2.php" class="form-inline">
+        <h5 autofocus style="text-align:center;">Filter Records By</h5>
+        <p style="text-align:center;">
+        <select autofocus id="Filtercheck" name="dropdown" onChange="datepicker()" class="select custom-select">
+            <option value="coursecode">Course Code</option>
+            <option value="coursename">Course Name</option>
+            <option value="shortname">Short Name</option>
+            <option value="startdate">Start Date</option>
+            <option value="enddate">End Date</option>
+        </select>
+        <input type="date" name="date" id="id_enddate" class="form-control">
+        <input id="inputcheck" name="inputtext" type="text" placeholder="Enter Value" class="form-control">
+        <input id="buttoncheck" type="submit" value="Search" class="btn btn-info">
+        <body onload="datepicker()">
+        </p>
+    </form>
+    <br />
+    <script>
+        var a=document.getElementById('Filtercheck').value;
+        document.getElementById('id_enddate').style.display="none";
+        function datepicker(){
+            var a=document.getElementById('Filtercheck').value;
+            if (a=='startdate' || a=='enddate'){
+                document.getElementById('inputcheck').style.display="none";
+                document.getElementById('id_enddate').style.display="inline";
+            }
+            else{
+                document.getElementById('inputcheck').style.display="inline";
+                document.getElementById('id_enddate').style.display="none";
+            }
+        }
+    </script>
+<?php
+
+	//Displaying all courses
+	$courses=$DB->get_records_sql('SELECT * FROM `mdl_course` WHERE id != ? ORDER BY id DESC', array(1));
+    
+    if($courses){
+        $i = 1;
+        foreach ($courses as $records){
+            $fullname = $records->fullname;
+            $shortname = $records->shortname;
+            $idnumber = $records->idnumber;
+            $id=$records->id;
+            echo "<h4><a href='./edit_course.php?course=$id' title='Edit Course'>$i. $fullname ($shortname $idnumber)</a></h4><br />";
+            $i++;
+        }
+    }
+    else{
+        echo "<h3>No courses found!</h3>";
+    }
+    echo $OUTPUT->footer();
+?>
