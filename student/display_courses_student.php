72080e7e66d129de34057aca68a0de210ef9115d
MussabTanveer <mussab.tanveer9@gmail.com>
2018-06-29 21:14:06 +0500

    include course selection template

--- student/display_courses_student.php
+++ student/display_courses_student.php
@@ -15,70 +15,18 @@
     ?>
     <script src="../script/jquery/jquery-3.2.1.js"></script>
     <?php
-
-    // Dispaly all courses
-    //$rec=$DB->get_records_sql('SELECT * FROM  `mdl_course` WHERE startdate != ? AND visible = ?', array( 0 , 1 ));
-    $rec=$DB->get_records_sql('SELECT c.id, c.fullname, c.shortname, c.idnumber
-    
-        FROM mdl_course c
-    
-        INNER JOIN mdl_context cx ON c.id = cx.instanceid
-    
-        AND cx.contextlevel = ?
-    
-        INNER JOIN mdl_role_assignments ra ON cx.id = ra.contextid
-    
-        INNER JOIN mdl_role r ON ra.roleid = r.id
-    
-        INNER JOIN mdl_user usr ON ra.userid = usr.id
-    
-        WHERE r.shortname = ?
-    
-        AND usr.id = ?', array('50', 'student', $USER->id));
-		
-    if($rec){
-        ?>
-        <form method="post" action="display_activities_student.php" id="form_check">
-        <?php
-        $serialno = 0;
-        $table = new html_table();
-        $table->head = array('S. No.','Full Name', 'Short Name' , 'Course Code', 'Select');
-        foreach ($rec as $records) {
-			$serialno++;
-            $id = $records->id;
-            $fname = $records->fullname;
-            $sname = $records->shortname;
-            $idnum = $records->idnumber;
-            $table->data[] = array($serialno, $fname, $sname, $idnum, '<input type="radio" value="'.$id.'" name="courseid">');
-        }
-        if($serialno == 1){
-            
-            global $SESSION;
-            $SESSION->cid4 = $id;
-        
-            redirect('display_activities_student.php');
-        }
-        echo html_writer::table($table);
-        ?>
-        <input type='submit' value='NEXT' name='submit' class="btn btn-primary">
-        </form>
-        <br />
-        <p id="msg"></p>
-
-        <script>
-        $('#form_check').on('submit', function (e) {
-            if ($("input[type=radio]:checked").length === 0) {
-                e.preventDefault();
-                $("#msg").html("<font color='red'>Select any one course!</font>");
-                return false;
-            }
-        });
-        </script>
-        <?php
-        echo $OUTPUT->footer();
+    if(isset($_POST['submit']) && isset( $_POST['semesterid']))
+    {
+        $semester_id=$_POST['semesterid'];
+        // Dispaly all courses
+        $action="display_activities_student.php";
+        require '../templates/display_courses_student_template.php';
     }
-    else{
-        echo "<h3>No courses found!</h3>";
-        echo $OUTPUT->footer();
+    else
+    {?>
+        <h2 style="color:red;"> Invalid Selection </h2>
+        <a href="./report_student.php">Back</a>
+    <?php
     }
+    echo $OUTPUT->footer();
 ?>
